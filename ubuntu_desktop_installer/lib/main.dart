d60c0ede2910d3caf917f0fd18aeccc74b14cdf5
Ken VanDine <ken.vandine@canonical.com>
2020-10-20 10:48:58 -0400

    Updated to use Yaru and dropped cupertino icons

--- ubuntu_desktop_installer/lib/main.dart
+++ ubuntu_desktop_installer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yaru/yaru.dart';
 
 import 'package:subiquity_client/subiquity_client.dart';
 
@@ -10,20 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    var theme = yaruTheme;
     return MaterialApp(
       title: 'Ubuntu Desktop Installer',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
+      theme: theme,
       home: MyHomePage(title: 'Ubuntu Desktop Installer'),
     );
   }
