a5b58fecc732b2f170a2db5c38ef18363f76d31b
Olivier Tilloy <olivier.tilloy@canonical.com>
2020-11-09 16:22:30 +0100

    Adjust the default size of the window to match design mockups.

--- ubuntu_desktop_installer/linux/my_application.cc
+++ ubuntu_desktop_installer/linux/my_application.cc
@@ -20,7 +20,7 @@ static void my_application_activate(GApplication* application) {
   gtk_header_bar_set_show_close_button(header_bar, TRUE);
   gtk_header_bar_set_decoration_layout(header_bar, ":close");
   gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
-  gtk_window_set_default_size(window, 800, 600);
+  gtk_window_set_default_size(window, 960, 680);
   gtk_widget_show(GTK_WIDGET(window));
 
   g_autoptr(FlDartProject) project = fl_dart_project_new();
