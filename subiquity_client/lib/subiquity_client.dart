5cdaa65db9dad52eba995e93dd7122cd3c9d2c54
Marcus Tomlinson <themarcustomlinson@gmail.com>
2020-11-10 18:57:05 +0000

    Add TestServer class with some basic tests (#2)

--- subiquity_client/lib/subiquity_client.dart
+++ subiquity_client/lib/subiquity_client.dart
@@ -1,5 +1,34 @@
 library subiquity_client;
 
+import 'package:http/http.dart';
+import 'package:subiquity_client/src/http_unix_client.dart';
+
 class SubiquityClient {
-  int test(int value) => value + 1;
+  HttpUnixClient _client;
+
+  SubiquityClient(String socket_path) {
+    _client = HttpUnixClient(socket_path);
+  }
+
+  void close() {
+    _client.close();
+  }
+
+  Future<String> status() async {
+    var request = Request('GET', Uri.http('localhost', 'meta/status'));
+    var response = await _client.send(request);
+    return response.stream.bytesToString();
+  }
+
+  Future<String> keyboard() async {
+    var request = Request('GET', Uri.http('localhost', 'keyboard'));
+    var response = await _client.send(request);
+    return response.stream.bytesToString();
+  }
+
+  Future<String> locale() async {
+    var request = Request('GET', Uri.http('localhost', 'locale'));
+    var response = await _client.send(request);
+    return response.stream.bytesToString();
+  }
 }
--- subiquity_client/test/subiquity_client_test.dart
+++ subiquity_client/test/subiquity_client_test.dart
@@ -1,13 +1,26 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:subiquity_client/subiquity_client.dart';
 
+import 'package:subiquity_client/test/test_server.dart';
+
 void main() {
-  test('adds one to input value', () {
-    final client = SubiquityClient();
-    expect(client.test(2), 3);
-    expect(client.test(-7), -6);
-    expect(client.test(0), 1);
-    expect(() => client.test(null), throwsNoSuchMethodError);
+  final _test_server = TestServer();
+  var _socket_path;
+
+  setUp(() async {
+    _socket_path = await _test_server.start('examples/simple.json');
+  });
+
+  tearDown(() async {
+    _test_server.stop();
+  });
+
+  test('simple server requests', () async {
+    final client = SubiquityClient(_socket_path);
+    expect(await client.status(), contains('"state": "INTERACTIVE"'));
+    expect(await client.keyboard(),
+        '{"layout": "us", "variant": "", "toggle": null}');
+    // expect(await client.locale(), '"en_US"');
+    client.close();
   });
 }
