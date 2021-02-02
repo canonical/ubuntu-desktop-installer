e93639dda7bf49af594a519a0a2c12ed2fa1d57f
Gioele Pannetto <gioele@pannetto.com>
2021-02-02 23:01:03 +0100

    Added full stops at the of sentences Added return type to the missing one

--- ubuntu_desktop_installer/lib/l10n/messages_it.dart
+++ ubuntu_desktop_installer/lib/l10n/messages_it.dart
@@ -19,9 +19,9 @@ typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
 class MessageLookup extends MessageLookupByLibrary {
   String get localeName => 'it';
 
-  static m0(url) => "Per istruzioni, apri questa pagina da un telefono o un altro dispositivo: <a href=\"https://${url}\">${url}</a>";
+  static String m0(url) => "Per istruzioni, apri questa pagina da un telefono o un altro dispositivo: <a href=\"https://${url}\">${url}</a>";
 
-  static m1(url) => "Forse vorresti leggere le <a href=\"${url}\">informazioni di rilascio</a>.";
+  static String m1(url) => "Forse vorresti leggere le <a href=\"${url}\">informazioni di rilascio</a>.";
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function> {
@@ -30,15 +30,15 @@ class MessageLookup extends MessageLookupByLibrary {
     "Install Ubuntu" : MessageLookupByLibrary.simpleMessage("Installa Ubuntu"),
     "Install Ubuntu alongside (or instead of) your current operating system. This shouldn\'t take too long." : MessageLookupByLibrary.simpleMessage("Installa Ubuntu accanto (o al posto di) al tuo sistema operativo attuale. Questo non dovrebbe durare molto."),
     "Repair installation" : MessageLookupByLibrary.simpleMessage("Ripara installazione"),
-    "Repairing will reinstall all installed software without touching documents or settings." : MessageLookupByLibrary.simpleMessage("La riparazione reinstallerá tutti i programmi senza toccare alcun documento o impostazione"),
+    "Repairing will reinstall all installed software without touching documents or settings." : MessageLookupByLibrary.simpleMessage("La riparazione reinstallerá tutti i programmi senza toccare alcun documento o impostazione."),
     "Restart" : MessageLookupByLibrary.simpleMessage("Riavvia"),
-    "This computer uses Intel RST (Rapid Storage Technology). You need to turn off RST in Windows before installing Ubuntu." : MessageLookupByLibrary.simpleMessage("Questo computer usa Intel RST (Rapid Storage Technology). Devi disattivare RST da Windows prima di poter installare Ubuntu"),
+    "This computer uses Intel RST (Rapid Storage Technology). You need to turn off RST in Windows before installing Ubuntu." : MessageLookupByLibrary.simpleMessage("Questo computer usa Intel RST (Rapid Storage Technology). Devi disattivare RST da Windows prima di poter installare Ubuntu."),
     "Try Ubuntu" : MessageLookupByLibrary.simpleMessage("Prova Ubuntu"),
     "Try or install" : MessageLookupByLibrary.simpleMessage("Prova o installa"),
     "Turn off RST" : MessageLookupByLibrary.simpleMessage("Disattiva RST"),
-    "Ubuntu Desktop Installer" : MessageLookupByLibrary.simpleMessage("Ubuntu Desktop Installer"),
+    "Ubuntu Desktop Installer" : MessageLookupByLibrary.simpleMessage("Installer di Ubuntu Desktop"),
     "Welcome" : MessageLookupByLibrary.simpleMessage("Benvenuto"),
-    "You can try Ubuntu without making any changes to your computer." : MessageLookupByLibrary.simpleMessage("Puoi provare Ubuntu senza apportare alcuna modifica al tuo computer"),
+    "You can try Ubuntu without making any changes to your computer." : MessageLookupByLibrary.simpleMessage("Puoi provare Ubuntu senza apportare alcuna modifica al tuo computer."),
     "instructions" : m0,
     "releaseNotesLabel" : m1
   };
