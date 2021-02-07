c3da856b56578611e6e2fa3c45085cde57605d46
Lennoard <lennoardrai@gmail.com>
2021-02-06 22:45:21 -0300

    reduced line length in messages_pt.dart

--- ubuntu_desktop_installer/lib/l10n/messages_pt.dart
+++ ubuntu_desktop_installer/lib/l10n/messages_pt.dart
@@ -27,21 +27,34 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "Continue" : MessageLookupByLibrary.simpleMessage("Continuar"),
-    "Go Back" : MessageLookupByLibrary.simpleMessage("Voltar"),
-    "Install Ubuntu" : MessageLookupByLibrary.simpleMessage("Instalar o Ubuntu"),
-    "Install Ubuntu alongside (or instead of) your current operating system. This shouldn\'t take too long." : MessageLookupByLibrary.simpleMessage("Instalar o Ubuntu ao lado do (ou em substituição ao) seu sistema operacional atual. Isto não deve demorar muito."),
-    "Repair installation" : MessageLookupByLibrary.simpleMessage("Reparar instalação"),
-    "Repairing will reinstall all installed software without touching documents or settings." : MessageLookupByLibrary.simpleMessage("A reparação reinstalará todos os softwares instalados sem alterar documentos ou configurações."),
-    "Restart" : MessageLookupByLibrary.simpleMessage("Reiniciar"),
-    "This computer uses Intel RST (Rapid Storage Technology). You need to turn off RST in Windows before installing Ubuntu." : MessageLookupByLibrary.simpleMessage("Este computador usa Intel RST (Rapid Storage Technology). Você precisa desligar a RST no Windows antes de instalar o Ubuntu."),
-    "Try Ubuntu" : MessageLookupByLibrary.simpleMessage("Experimentar o Ubuntu"),
-    "Try or install" : MessageLookupByLibrary.simpleMessage("Experimentar ou instalar"),
-    "Turn off RST" : MessageLookupByLibrary.simpleMessage("Desligue a RST"),
-    "Ubuntu Desktop Installer" : MessageLookupByLibrary.simpleMessage("Instalador do Ubuntu Desktop"),
-    "Welcome" : MessageLookupByLibrary.simpleMessage("Bem-vindo"),
-    "You can try Ubuntu without making any changes to your computer." : MessageLookupByLibrary.simpleMessage("Você pode experimentar o Ubuntu sem fazer nenhuma alteração no seu computador."),
-    "instructions" : m0,
-    "releaseNotesLabel" : m1
-  };
+        "Continue": MessageLookupByLibrary.simpleMessage("Continuar"),
+        "Go Back": MessageLookupByLibrary.simpleMessage("Voltar"),
+        "Install Ubuntu":
+            MessageLookupByLibrary.simpleMessage("Instalar o Ubuntu"),
+        "Install Ubuntu alongside (or instead of) your current operating system. This shouldn\'t take too long.":
+            MessageLookupByLibrary.simpleMessage(
+                "Instalar o Ubuntu ao lado do (ou em substituição ao) seu sistema operacional atual. Isto não deve demorar muito."),
+        "Repair installation":
+            MessageLookupByLibrary.simpleMessage("Reparar instalação"),
+        "Repairing will reinstall all installed software without touching documents or settings.":
+            MessageLookupByLibrary.simpleMessage(
+                "A reparação reinstalará todos os softwares instalados sem alterar documentos ou configurações."),
+        "Restart": MessageLookupByLibrary.simpleMessage("Reiniciar"),
+        "This computer uses Intel RST (Rapid Storage Technology). You need to turn off RST in Windows before installing Ubuntu.":
+            MessageLookupByLibrary.simpleMessage(
+                "Este computador usa Intel RST (Rapid Storage Technology). Você precisa desligar a RST no Windows antes de instalar o Ubuntu."),
+        "Try Ubuntu":
+            MessageLookupByLibrary.simpleMessage("Experimentar o Ubuntu"),
+        "Try or install":
+            MessageLookupByLibrary.simpleMessage("Experimentar ou instalar"),
+        "Turn off RST": MessageLookupByLibrary.simpleMessage("Desligue a RST"),
+        "Ubuntu Desktop Installer": MessageLookupByLibrary.simpleMessage(
+            "Instalador do Ubuntu Desktop"),
+        "Welcome": MessageLookupByLibrary.simpleMessage("Bem-vindo"),
+        "You can try Ubuntu without making any changes to your computer.":
+            MessageLookupByLibrary.simpleMessage(
+                "Você pode experimentar o Ubuntu sem fazer nenhuma alteração no seu computador."),
+        "instructions": m0,
+        "releaseNotesLabel": m1
+      };
 }
