22893f217c7f6412e78c0c89a8943a0c3fe05ad4
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-03-10 17:24:48 +0100

    Fix an incorrect import.

--- ubuntu_desktop_installer/lib/localized_view.dart
+++ ubuntu_desktop_installer/lib/localized_view.dart
@@ -1,16 +1,12 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-///
 typedef LocalizedViewBuilder = Widget Function(
     BuildContext context, AppLocalizations lang);
 
-///
 class LocalizedView extends StatelessWidget {
-  ///
   final LocalizedViewBuilder builder;
 
-  ///
   LocalizedView({@required this.builder});
 
   @override
