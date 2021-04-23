7fd91e1661456967a9d385365aaf45f35ded7470
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-04-23 23:40:14 +0200

    Widget tests for the Welcome page.

---
+++ packages/ubuntu_desktop_installer/test/welcome_page_test.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:ubuntu_desktop_installer/app.dart';
+import 'package:ubuntu_desktop_installer/keyboard_model.dart';
+import 'package:ubuntu_desktop_installer/l10n/app_localizations.dart';
+import 'package:ubuntu_desktop_installer/pages/welcome_page.dart';
+import 'package:ubuntu_desktop_installer/routes.dart';
+
+class MockNavigatorObserver extends NavigatorObserver {
+  List<Route> pushed = [];
+
+  @override
+  void didPush(Route route, Route previousRoute) => pushed.add(route);
+}
+
+void main() {
+  MockNavigatorObserver observer;
+  MaterialApp app;
+
+  setUpAll(() async {
+    await setupAppLocalizations();
+  });
+
+  void setUpApp(WidgetTester tester) async {
+    observer = MockNavigatorObserver();
+    app = MaterialApp(
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: Locale('en'),
+      home: WelcomePage(),
+      navigatorObservers: [observer],
+      routes: <String, WidgetBuilder>{
+        Routes.tryOrInstall: (context) => null,
+      },
+    );
+    await tester.pumpWidget(ChangeNotifierProvider(
+        create: (context) => KeyboardModel(), child: app));
+    expect(observer.pushed.length, 1);
+    expect(observer.pushed.first.settings.name, '/');
+  }
+
+  tearDown(() {
+    app = null;
+    observer = null;
+  });
+
+  testWidgets('should display a list of languages', (tester) async {
+    await setUpApp(tester);
+
+    final languageList = find.byType(ListView);
+    expect(languageList, findsOneWidget);
+
+    final listItems =
+        find.descendant(of: languageList, matching: find.byType(ListTile));
+    expect(listItems, findsWidgets);
+    expect(listItems.evaluate().length <= app.supportedLocales.length, true);
+    for (final language in ['English', 'Français', 'Italiano']) {
+      expect(find.descendant(of: languageList, matching: find.text(language)),
+          findsOneWidget);
+    }
+
+    final itemEnglish = find.descendant(
+        of: languageList, matching: find.widgetWithText(ListTile, 'English'));
+    expect(itemEnglish, findsOneWidget);
+    expect((itemEnglish.evaluate().single.widget as ListTile).selected, true);
+    expect(UbuntuDesktopInstallerApp.locale.languageCode, 'en');
+
+    final itemFrench = find.descendant(
+        of: languageList, matching: find.widgetWithText(ListTile, 'Français'));
+    expect(itemFrench, findsOneWidget);
+    expect((itemFrench.evaluate().single.widget as ListTile).selected, false);
+
+    await tester.tap(itemFrench);
+    await tester.pump();
+    expect((itemEnglish.evaluate().single.widget as ListTile).selected, false);
+    expect((itemFrench.evaluate().single.widget as ListTile).selected, true);
+    expect(UbuntuDesktopInstallerApp.locale.languageCode, 'fr');
+  });
+
+  testWidgets('should continue to next page', (tester) async {
+    await setUpApp(tester);
+
+    final continueButton = find.widgetWithText(OutlinedButton, 'Continue');
+    expect(continueButton, findsOneWidget);
+
+    await tester.tap(continueButton);
+
+    expect(observer.pushed.length, 2);
+    expect(observer.pushed.last.settings.name, Routes.tryOrInstall);
+  });
+}
