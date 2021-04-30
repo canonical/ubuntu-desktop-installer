48aee0bd7a5a22c3f5803723b2a91564864dfb25
J-P Nurmi <jpnurmi@gmail.com>
2021-04-30 20:10:45 +0200

    Split RoundedContainer & RoundedListView

--- packages/ubuntu_desktop_installer/lib/widgets.dart
+++ packages/ubuntu_desktop_installer/lib/widgets.dart
@@ -1,2 +1,3 @@
 export 'widgets/localized_view.dart';
+export 'widgets/rounded_container.dart';
 export 'widgets/rounded_list_view.dart';
--- packages/ubuntu_desktop_installer/lib/widgets/rounded_list_view.dart
+++ packages/ubuntu_desktop_installer/lib/widgets/rounded_list_view.dart
@@ -1,57 +1,50 @@
 import 'package:flutter/material.dart';
 
-const double _kDefaultBorderWidth = 1.0;
-const double _kDefaultBorderRadius = 5.0;
+import 'rounded_container.dart';
 
 /// A scrollable list view with a rounded border.
 class RoundedListView extends StatelessWidget {
   /// Creates a scrollable list of widgets that are created on demand.
   const RoundedListView.builder({
     Key? key,
-    required ScrollController controller,
-    required int itemCount,
-    required IndexedWidgetBuilder itemBuilder,
-    Color? borderColor,
-    double? borderWidth,
-    double? borderRadius,
-  })  : _controller = controller,
-        _itemCount = itemCount,
-        _itemBuilder = itemBuilder,
-        _borderColor = borderColor,
-        _borderWidth = borderWidth,
-        _borderRadius = borderRadius,
-        super(key: key);
+    this.controller,
+    required this.itemCount,
+    required this.itemBuilder,
+    this.borderColor,
+    this.borderWidth,
+    this.borderRadius,
+  }) : super(key: key);
 
-  final ScrollController _controller;
-  final int _itemCount;
-  final IndexedWidgetBuilder _itemBuilder;
-  final Color? _borderColor;
-  final double? _borderWidth;
-  final double? _borderRadius;
+  /// See [ListView.controller]
+  final ScrollController? controller;
+
+  /// See [ListView.builder]
+  final int itemCount;
+
+  /// See [ListView.builder]
+  final IndexedWidgetBuilder itemBuilder;
+
+  /// See [BoxDecoration.border]
+  final Color? borderColor;
+
+  /// See [BoxDecoration.border]
+  final double? borderWidth;
+
+  /// See [BoxDecoration.borderRadius]
+  final double? borderRadius;
 
   @override
   Widget build(BuildContext context) {
-    final borderWidth = _borderWidth ?? _kDefaultBorderWidth;
-    final borderRadius = _borderRadius ?? _kDefaultBorderRadius;
-    return Container(
-      decoration: BoxDecoration(
-        border: Border.all(
-          color: _borderColor ?? Theme.of(context).dividerColor,
-          width: borderWidth,
-        ),
-        borderRadius: BorderRadius.circular(borderRadius),
-      ),
+    return RoundedContainer(
+      borderColor: borderColor,
+      borderWidth: borderWidth,
+      borderRadius: borderRadius,
       child: Scrollbar(
-        controller: _controller,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(borderRadius - borderWidth / 2),
-          child: Material(
-            child: ListView.builder(
-              controller: _controller,
-              itemCount: _itemCount,
-              itemBuilder: _itemBuilder,
-            ),
-          ),
+        controller: controller,
+        child: ListView.builder(
+          controller: controller,
+          itemCount: itemCount,
+          itemBuilder: itemBuilder,
         ),
       ),
     );
