6e81031176da80ff8beb253131a34da0b11a25a0
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-05-12 11:35:31 +0200

    Instantiate and run a test server when starting the installer.

--- packages/ubuntu_desktop_installer/lib/main.dart
+++ packages/ubuntu_desktop_installer/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:subiquity_client/subiquity_client.dart';
+import 'package:subiquity_client/test/test_server.dart';
 
 import 'app.dart';
 import 'keyboard_model.dart';
 import 'l10n/app_localizations.dart';
 
 Future<void> main() async {
+  final subiquityClient = SubiquityClient();
+
+  final testServer = TestServer();
+  await testServer.start('examples/simple.json').then(subiquityClient.open);
+
   WidgetsFlutterBinding.ensureInitialized();
   await setupAppLocalizations();
   runApp(MultiProvider(
     providers: [
-      Provider(create: (_) => SubiquityClient()),
+      Provider(
+          create: (_) => subiquityClient,
+          lazy: false,
+          dispose: (_, __) {
+            subiquityClient.close();
+            testServer.stop();
+          }),
       ChangeNotifierProvider(create: (_) => KeyboardModel()),
     ],
     child: UbuntuDesktopInstallerApp(),
