8827023f2cb6fbc048ec611788c07d6ba7dcdb23
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-06-01 17:27:09 +0200

    Rebase screen 6 ("Merge back changes from the firefox.focal stable branch") on WizardPage.

--- packages/ubuntu_desktop_installer/lib/pages/updates_other_software/updates_other_software_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/updates_other_software/updates_other_software_page.dart
@@ -3,9 +3,8 @@ import 'package:provider/provider.dart';
 
 import '../../widgets.dart';
 import '../../widgets/localized_view.dart';
+import '../wizard_page.dart';
 import 'updates_other_software_model.dart';
-
-const _kButtonPadding = EdgeInsets.only(right: 20, bottom: 20);
 
 class UpdatesOtherSoftwarePage extends StatefulWidget {
   @override
@@ -26,74 +25,56 @@ class _UpdatesOtherSoftwarePageState extends State<UpdatesOtherSoftwarePage> {
   Widget build(BuildContext context) {
     final model = context.watch<UpdateOtherSoftwareModel>();
     return LocalizedView(
-      builder: (context, lang) => Scaffold(
-        appBar: AppBar(
-          title: Text(lang.updatesOtherSoftwarePageTitle),
-          automaticallyImplyLeading: false,
+      builder: (context, lang) => WizardPage(
+        title: Text(lang.updatesOtherSoftwarePageTitle),
+        content: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(lang.updatesOtherSoftwarePageDescription),
+            ),
+            RadioListTile<InstallationMode>(
+              title: Text(lang.normalInstallationTitle),
+              subtitle: Text(lang.normalInstallationSubtitle),
+              contentPadding: const EdgeInsets.only(left: 10),
+              value: InstallationMode.normal,
+              groupValue: model.installationMode,
+              onChanged: model.setInstallationMode,
+            ),
+            RadioListTile<InstallationMode>(
+              title: Text(lang.minimalInstallationTitle),
+              subtitle: Text(lang.minimalInstallationSubtitle),
+              value: InstallationMode.minimal,
+              contentPadding: const EdgeInsets.only(left: 10),
+              groupValue: model.installationMode,
+              onChanged: model.setInstallationMode,
+            ),
+            const SizedBox(height: 24),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(lang.otherOptions),
+            ),
+            CheckboxListTile(
+              title: Text(lang.installThirdPartyTitle),
+              subtitle: Text(lang.installThirdPartySubtitle),
+              controlAffinity: ListTileControlAffinity.leading,
+              contentPadding: const EdgeInsets.only(left: 10),
+              value: model.installThirdParty,
+              onChanged: model.setInstallThirdParty,
+            )
+          ],
         ),
-        body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20, top: 20, bottom: 8),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(lang.updatesOtherSoftwarePageDescription),
-                ),
-              ),
-              RadioListTile<InstallationMode>(
-                title: Text(lang.normalInstallationTitle),
-                subtitle: Text(lang.normalInstallationSubtitle),
-                contentPadding: const EdgeInsets.only(left: 20),
-                value: InstallationMode.normal,
-                groupValue: model.installationMode,
-                onChanged: model.setInstallationMode,
-              ),
-              RadioListTile<InstallationMode>(
-                title: Text(lang.minimalInstallationTitle),
-                subtitle: Text(lang.minimalInstallationSubtitle),
-                value: InstallationMode.minimal,
-                contentPadding: const EdgeInsets.only(left: 20),
-                groupValue: model.installationMode,
-                onChanged: model.setInstallationMode,
-              ),
-              const SizedBox(height: 24),
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-                child: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(lang.otherOptions),
-                ),
-              ),
-              CheckboxListTile(
-                title: Text(lang.installThirdPartyTitle),
-                subtitle: Text(lang.installThirdPartySubtitle),
-                controlAffinity: ListTileControlAffinity.leading,
-                contentPadding: const EdgeInsets.only(left: 20),
-                value: model.installThirdParty,
-                onChanged: model.setInstallThirdParty,
-              )
-            ],
+        actions: <WizardAction>[
+          WizardAction(
+            label: lang.backButtonText,
+            onActivated: Navigator.of(context).pop,
           ),
-        ),
-        bottomNavigationBar: Padding(
-          padding: _kButtonPadding,
-          child: ButtonBar(
-            children: <Widget>[
-              OutlinedButton(
-                child: Text(lang.backButtonText),
-                onPressed: Navigator.of(context).pop,
-              ),
-              OutlinedButton(
-                child: Text(lang.continueButtonText),
-                onPressed: () {
-                  // TODO: add next step here
-                },
-              ),
-            ],
+          WizardAction(
+            label: lang.continueButtonText,
+            onActivated: () {},
           ),
-        ),
+        ],
       ),
     );
   }
