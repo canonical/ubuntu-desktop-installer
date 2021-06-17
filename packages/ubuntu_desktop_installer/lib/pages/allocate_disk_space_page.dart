71dc9ff5a525f5f82c96c3dd2719b8a7dff2cf7b
Olivier Tilloy <olivier.tilloy@canonical.com>
2021-06-17 16:01:54 +0200

    Rebase on WizardPage.

--- packages/ubuntu_desktop_installer/lib/pages/allocate_disk_space_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/allocate_disk_space_page.dart
@@ -5,6 +5,7 @@ import 'package:subiquity_client/subiquity_client.dart';
 
 import '../routes.dart';
 import '../widgets.dart';
+import 'wizard_page.dart';
 
 class _DiskOrPartition {
   final Disk disk;
@@ -55,95 +56,86 @@ class _AllocateDiskSpacePageState extends State<AllocateDiskSpacePage> {
   @override
   Widget build(BuildContext context) {
     return LocalizedView(
-        builder: (context, lang) => Scaffold(
-            appBar: AppBar(
+        builder: (context, lang) => WizardPage(
               title: Text(lang.allocateDiskSpace),
-              automaticallyImplyLeading: false,
-            ),
-            body: Container(
-                padding: const EdgeInsets.all(20),
-                child: Column(children: <Widget>[
-                  Expanded(
-                      child: Column(children: <Widget>[
-                    RoundedContainer(
-                      child: DataTable(
-                        headingTextStyle: Theme.of(context)
-                            .textTheme
-                            .caption!
-                            .apply(fontWeightDelta: 3, color: Colors.black),
-                        dataTextStyle: Theme.of(context)
-                            .textTheme
-                            .caption!
-                            .apply(color: Colors.black),
-                        columns: <DataColumn>[
-                          DataColumn(
-                            label: Text(lang.diskHeadersDevice),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersType),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersMountPoint),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersSize),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersUsed),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersSystem),
-                          ),
-                          DataColumn(
-                            label: Text(lang.diskHeadersFormat),
-                          ),
-                        ],
-                        rows: List<DataRow>.generate(_disksAndPartitions.length,
-                            (index) {
-                          final element = _disksAndPartitions[index];
-                          return DataRow(cells: <DataCell>[
-                            DataCell(Row(children: [
-                              ImageIcon(AssetImage(
-                                  'assets/icon-hard-drive-solid.png')),
-                              const SizedBox(width: 16),
-                              Text(element.name),
-                            ])),
-                            DataCell(Text(element.disk.type!)),
-                            DataCell(Text('')),
-                            DataCell(Text(filesize(element.disk.size))),
-                            DataCell(Text('')),
-                            DataCell(Text('')),
-                            DataCell(Checkbox(value: true, onChanged: null)),
-                          ]);
-                        }),
-                      ),
+              content: Column(children: <Widget>[
+                Expanded(
+                    child: Column(children: <Widget>[
+                  RoundedContainer(
+                    child: DataTable(
+                      headingTextStyle: Theme.of(context)
+                          .textTheme
+                          .caption!
+                          .apply(fontWeightDelta: 3, color: Colors.black),
+                      dataTextStyle: Theme.of(context)
+                          .textTheme
+                          .caption!
+                          .apply(color: Colors.black),
+                      columns: <DataColumn>[
+                        DataColumn(
+                          label: Text(lang.diskHeadersDevice),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersType),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersMountPoint),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersSize),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersUsed),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersSystem),
+                        ),
+                        DataColumn(
+                          label: Text(lang.diskHeadersFormat),
+                        ),
+                      ],
+                      rows: List<DataRow>.generate(_disksAndPartitions.length,
+                          (index) {
+                        final element = _disksAndPartitions[index];
+                        return DataRow(cells: <DataCell>[
+                          DataCell(Row(children: [
+                            ImageIcon(
+                                AssetImage('assets/icon-hard-drive-solid.png')),
+                            const SizedBox(width: 16),
+                            Text(element.name),
+                          ])),
+                          DataCell(Text(element.disk.type!)),
+                          DataCell(Text('')),
+                          DataCell(Text(filesize(element.disk.size))),
+                          DataCell(Text('')),
+                          DataCell(Text('')),
+                          DataCell(Checkbox(value: true, onChanged: null)),
+                        ]);
+                      }),
                     ),
-                  ])),
-                  const SizedBox(height: 20),
-                  ButtonBar(
-                    children: <OutlinedButton>[
-                      OutlinedButton(
-                        child: Text(lang.backButtonText),
-                        onPressed: Navigator.of(context).pop,
-                      ),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                            backgroundColor: const Color(0xFF0E8420),
-                            primary: Colors.white),
-                        child: Text(lang.startInstallingButtonText),
-                        onPressed: () {
-                          final client = Provider.of<SubiquityClient>(context,
-                              listen: false);
-                          final choice = GuidedChoice(
-                              diskId: _response!.disks![0].id, useLvm: false);
-                          client.setGuidedStorage(choice).then(
-                              (r) => print('Storage response: ${r.toJson()}'));
-                          Navigator.pushNamed(
-                              context, Routes.updatesOtherSoftware);
-                        },
-                      ),
-                    ],
                   ),
-                ]))));
+                ])),
+              ]),
+              actions: <WizardAction>[
+                WizardAction(
+                  label: lang.backButtonText,
+                  onActivated: Navigator.of(context).pop,
+                ),
+                WizardAction(
+                  label: lang.startInstallingButtonText,
+                  highlighted: true,
+                  onActivated: () {
+                    final client =
+                        Provider.of<SubiquityClient>(context, listen: false);
+                    final choice = GuidedChoice(
+                        diskId: _response!.disks![0].id, useLvm: false);
+                    client
+                        .setGuidedStorage(choice)
+                        .then((r) => print('Storage response: ${r.toJson()}'));
+                    Navigator.pushNamed(context, Routes.updatesOtherSoftware);
+                  },
+                ),
+              ],
+            ));
   }
 }
