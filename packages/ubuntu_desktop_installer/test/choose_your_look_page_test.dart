8ddc28247f4b3ccd2a96ef9b22efc5272019d043
Feichtmeier <frederik.feichtmeier@gmail.com>
2021-07-03 13:00:51 +0200

    Add choose_your_look_page_test

---
+++ packages/ubuntu_desktop_installer/test/choose_your_look_page_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:gsettings/gsettings.dart';
+import 'package:provider/provider.dart';
+import 'package:ubuntu_desktop_installer/app_theme.dart';
+
+import 'package:ubuntu_desktop_installer/pages/choose_your_look_page.dart';
+import 'package:ubuntu_desktop_installer/widgets/option_card.dart';
+
+import 'simple_navigator_observer.dart';
+
+void main() {
+  final themeSettings = GSettings(schemaId: 'org.gnome.desktop.interface');
+
+  late SimpleNavigatorObserver observer;
+  late MaterialApp app;
+
+  Future<void> setUpApp(WidgetTester tester) async {
+    observer = SimpleNavigatorObserver();
+    app = MaterialApp(
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      locale: Locale('en'),
+      home: ChangeNotifierProvider(
+        create: (_) => AppTheme(themeSettings),
+        child: ChooseYourLookPage(),
+      ),
+      navigatorObservers: [observer],
+    );
+    await tester.pumpWidget(app);
+    expect(observer.pushed.length, 1);
+    expect(observer.pushed.first.settings.name, '/');
+  }
+
+  testWidgets('should toggle the theme', (tester) async {
+    await setUpApp(tester);
+
+    expect(find.byType(AppBar), findsNWidgets(1));
+    expect(find.byType(Text), findsNWidgets(8));
+    expect(find.byType(OptionCard), findsNWidgets(2));
+    expect(find.byType(OutlinedButton), findsNWidgets(2));
+  });
+}
