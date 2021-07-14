23c112843e91fb0e006956393ba0774ac3cd0745
J-P Nurmi <jp.nurmi@canonical.com>
2021-07-14 09:57:52 +0200

    Remove KeyboardModel consumer

--- packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_page.dart
@@ -66,91 +66,89 @@ class _KeyboardLayoutPageState extends State<KeyboardLayoutPage> {
   @override
   Widget build(BuildContext context) {
     final model = Provider.of<KeyboardLayoutModel>(context);
-    return Consumer<KeyboardModel>(builder: (context, keyboardModel, child) {
-      return LocalizedView(
-        builder: (context, lang) => WizardPage(
-          title: Text(lang.keyboardLayoutPageTitle),
-          header: Text(lang.chooseYourKeyboardLayout),
-          content: Column(
-            children: <Widget>[
-              Expanded(
-                child: Row(
-                  children: <Widget>[
-                    Expanded(
-                      child: RoundedListView.builder(
-                        controller: _layoutListScrollController,
-                        itemCount: model.layoutCount,
-                        itemBuilder: (context, index) {
-                          return AutoScrollTag(
-                            index: index,
-                            key: ValueKey(index),
-                            controller: _layoutListScrollController,
-                            child: ListTile(
-                              title: Text(model.layoutName(index)),
-                              selected: index == model.selectedLayoutIndex,
-                              onTap: () => model.selectLayout(index),
-                            ),
-                          );
-                        },
-                      ),
+    return LocalizedView(
+      builder: (context, lang) => WizardPage(
+        title: Text(lang.keyboardLayoutPageTitle),
+        header: Text(lang.chooseYourKeyboardLayout),
+        content: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: RoundedListView.builder(
+                      controller: _layoutListScrollController,
+                      itemCount: model.layoutCount,
+                      itemBuilder: (context, index) {
+                        return AutoScrollTag(
+                          index: index,
+                          key: ValueKey(index),
+                          controller: _layoutListScrollController,
+                          child: ListTile(
+                            title: Text(model.layoutName(index)),
+                            selected: index == model.selectedLayoutIndex,
+                            onTap: () => model.selectLayout(index),
+                          ),
+                        );
+                      },
                     ),
-                    const SizedBox(width: 20),
-                    Expanded(
-                      child: RoundedListView.builder(
-                        controller: _keyboardVariantListScrollController,
-                        itemCount: model.variantCount,
-                        itemBuilder: (context, index) {
-                          return AutoScrollTag(
-                            index: index,
-                            key: ValueKey(index),
-                            controller: _keyboardVariantListScrollController,
-                            child: ListTile(
-                              title: Text(model.variantName(index)),
-                              selected: index == model.selectedVariantIndex,
-                              onTap: () => model.selectVariant(index),
-                            ),
-                          );
-                        },
-                      ),
+                  ),
+                  const SizedBox(width: 20),
+                  Expanded(
+                    child: RoundedListView.builder(
+                      controller: _keyboardVariantListScrollController,
+                      itemCount: model.variantCount,
+                      itemBuilder: (context, index) {
+                        return AutoScrollTag(
+                          index: index,
+                          key: ValueKey(index),
+                          controller: _keyboardVariantListScrollController,
+                          child: ListTile(
+                            title: Text(model.variantName(index)),
+                            selected: index == model.selectedVariantIndex,
+                            onTap: () => model.selectVariant(index),
+                          ),
+                        );
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(),
-                  hintText: lang.typeToTest,
-                ),
-              ),
-              const SizedBox(height: 20),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: OutlinedButton(
-                  child: Text(lang.detectLayout),
-                  onPressed: () {
-                    print('TODO: show dialog to detect keyboard layout');
-                  },
-                ),
-              ),
-            ],
-          ),
-          actions: <WizardAction>[
-            WizardAction(
-              label: lang.backButtonText,
-              onActivated: Navigator.of(context).pop,
             ),
-            WizardAction(
-              label: lang.continueButtonText,
-              enabled: model.isValid,
-              onActivated: () async {
-                await model.applyKeyboardSettings();
-                Navigator.pushNamed(context, Routes.updatesOtherSoftware);
-              },
+            const SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                hintText: lang.typeToTest,
+              ),
+            ),
+            const SizedBox(height: 20),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: OutlinedButton(
+                child: Text(lang.detectLayout),
+                onPressed: () {
+                  print('TODO: show dialog to detect keyboard layout');
+                },
+              ),
             ),
           ],
         ),
-      );
-    });
+        actions: <WizardAction>[
+          WizardAction(
+            label: lang.backButtonText,
+            onActivated: Navigator.of(context).pop,
+          ),
+          WizardAction(
+            label: lang.continueButtonText,
+            enabled: model.isValid,
+            onActivated: () async {
+              await model.applyKeyboardSettings();
+              Navigator.pushNamed(context, Routes.updatesOtherSoftware);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
