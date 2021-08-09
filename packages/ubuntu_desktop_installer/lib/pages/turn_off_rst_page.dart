4d043fbde5df1f4a13ef98520ca07dda0085da66
J-P Nurmi <jp.nurmi@canonical.com>
2021-08-09 16:07:20 +0200

    Update Turn off RST
    - sync spacing with the design
    - switch from PNG to SVG QR code
    - cleanup superfluous Column & Expanded widgets

--- packages/ubuntu_desktop_installer/lib/pages/turn_off_rst_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/turn_off_rst_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_html/style.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:wizard_router/wizard_router.dart';
 
@@ -24,28 +25,19 @@ class TurnOffRSTPage extends StatelessWidget {
           header: Text(lang.turnOffRSTDescription),
           content: Column(
             children: <Widget>[
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Align(
-                      alignment: Alignment.centerLeft,
-                      child: Html(
-                        data: lang.instructionsForRST('help.ubuntu.com/rst'),
-                        style: {
-                          'body': Style(
-                            margin: EdgeInsets.all(0),
-                          ),
-                        },
-                        onLinkTap: (url, _, __, ___) => launch(url!),
-                      ),
-                    ),
-                    const SizedBox(height: 40),
-                    Image.asset(
-                      'assets/rst.png',
-                      color: Theme.of(context).textTheme.bodyText1!.color,
-                    ),
-                  ],
-                ),
+              Html(
+                data: lang.instructionsForRST('help.ubuntu.com/rst'),
+                style: {
+                  'body': Style(
+                    margin: EdgeInsets.all(0),
+                  ),
+                },
+                onLinkTap: (url, _, __, ___) => launch(url!),
+              ),
+              const SizedBox(height: 60),
+              SvgPicture.asset(
+                'assets/rst.svg',
+                color: Theme.of(context).textTheme.bodyText1!.color,
               ),
             ],
           ),
