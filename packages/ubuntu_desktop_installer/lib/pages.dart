ba56581fe4ecf54acfe9e7375c8079c0aa00963b
J-P Nurmi <jp.nurmi@canonical.com>
2021-08-09 14:50:19 +0200

    Define the Turn off BitLocker page route for the sake of testing
    Note: this is not the final page order. For now, this particular order
    makes the page conveniently skipped (together) with the similar Turn
    off RST page. In order to test the page, specify the desired initial
    page route.

--- packages/ubuntu_desktop_installer/lib/pages.dart
+++ packages/ubuntu_desktop_installer/lib/pages.dart
@@ -4,6 +4,7 @@ export 'pages/installation_complete_page.dart';
 export 'pages/installation_slides/installation_slides_page.dart';
 export 'pages/keyboard_layout/keyboard_layout_page.dart';
 export 'pages/try_or_install/try_or_install_page.dart';
+export 'pages/turn_off_bitlocker_page.dart';
 export 'pages/turn_off_rst_page.dart';
 export 'pages/updates_other_software/updates_other_software_page.dart';
 export 'pages/welcome/welcome_page.dart';
