7487c0a68a85f1423f306b0b883cbd4b94a1b362
J-P Nurmi <jp.nurmi@canonical.com>
2021-08-10 13:29:22 +0200

    Add test for TurnOffRSTPage

---
+++ packages/ubuntu_desktop_installer/test/turn_off_rst_page_test.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:ubuntu_desktop_installer/pages/turn_off_rst/turn_off_rst_model.dart';
+import 'package:ubuntu_desktop_installer/pages/turn_off_rst/turn_off_rst_page.dart';
+import 'package:wizard_router/wizard_router.dart';
+
+import 'turn_off_rst_page_test.mocks.dart';
+
+extension LangTester<T> on WidgetTester {
+  AppLocalizations get lang {
+    final page = element(find.byType(TurnOffRSTPage));
+    return AppLocalizations.of(page)!;
+  }
+}
+
+@GenerateMocks([TurnOffRSTModel])
+void main() {
+  testWidgets('restart', (tester) async {
+    final model = MockTurnOffRSTModel();
+
+    await tester.pumpWidget(
+      MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        home: Wizard(
+          routes: {
+            '/': (_) {
+              return Provider<TurnOffRSTModel>.value(
+                value: model,
+                child: TurnOffRSTPage(),
+              );
+            },
+          },
+          onNext: (settings) => '/',
+        ),
+      ),
+    );
+
+    final restartButton = find.widgetWithText(
+      ElevatedButton,
+      tester.lang.restartButtonText,
+    );
+    expect(restartButton, findsOneWidget);
+
+    await tester.tap(restartButton);
+    verify(model.reboot()).called(1);
+  });
+}
