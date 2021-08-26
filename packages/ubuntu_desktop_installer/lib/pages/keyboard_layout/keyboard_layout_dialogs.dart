6ce77cbf24eb871cf1658d9843e9334a0ddaf3e8
J-P Nurmi <jp.nurmi@canonical.com>
2021-08-26 13:43:22 +0200

    Cleanup unused import

--- packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_dialogs.dart
+++ packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_dialogs.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_wizard/constants.dart';
