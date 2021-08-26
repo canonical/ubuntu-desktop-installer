05c0724fd6a6183621ffbc47f6a5319491c8455b
J-P Nurmi <jp.nurmi@canonical.com>
2021-08-26 17:21:00 +0200

    Add WSL Profile setup page (#254)
    Close: #156

--- packages/ubuntu_wsl_setup/lib/l10n/app_localizations_en.dart
+++ packages/ubuntu_wsl_setup/lib/l10n/app_localizations_en.dart
@@ -38,6 +38,36 @@ class AppLocalizationsEn extends AppLocalizations {
   String get profileSetupTitle => 'Profile setup';
 
   @override
+  String get profileSetupHeader => 'Please create a default UNIX user account. For more information visit: <a href=\"https://aka.ms/wslusers\">https://aka.ms/wslusers</a>';
+
+  @override
+  String get profileSetupUsernameHint => 'Pick a username';
+
+  @override
+  String get profileSetupUsernameHelper => 'The username does not need to match your windows username.';
+
+  @override
+  String get profileSetupPasswordHint => 'Choose a password';
+
+  @override
+  String get profileSetupConfirmPasswordHint => 'Confirm your password';
+
+  @override
+  String get profileSetupShowAdvancedOptions => 'Show advanced options next page';
+
+  @override
+  String get profileSetupPasswordMismatch => 'The passwords do not match';
+
+  @override
+  String get profileSetupUsernameRequired => 'A username is required';
+
+  @override
+  String get profileSetupUsernameInvalid => 'The username is invalid';
+
+  @override
+  String get profileSetupPasswordRequired => 'A password is required';
+
+  @override
   String get advancedSetupTitle => 'Advanced setup';
 
   @override
@@ -80,6 +110,36 @@ class AppLocalizationsEnUs extends AppLocalizationsEn {
 
   @override
   String get profileSetupTitle => 'Profile setup';
+
+  @override
+  String get profileSetupHeader => 'Please create a default UNIX user account. For more information visit: <a href=\"https://aka.ms/wslusers\">https://aka.ms/wslusers</a>';
+
+  @override
+  String get profileSetupUsernameHint => 'Pick a username';
+
+  @override
+  String get profileSetupUsernameHelper => 'The username does not need to match your windows username.';
+
+  @override
+  String get profileSetupPasswordHint => 'Choose a password';
+
+  @override
+  String get profileSetupConfirmPasswordHint => 'Confirm your password';
+
+  @override
+  String get profileSetupShowAdvancedOptions => 'Show advanced options next page';
+
+  @override
+  String get profileSetupPasswordMismatch => 'The passwords do not match';
+
+  @override
+  String get profileSetupUsernameRequired => 'A username is required';
+
+  @override
+  String get profileSetupUsernameInvalid => 'The username is invalid';
+
+  @override
+  String get profileSetupPasswordRequired => 'A password is required';
 
   @override
   String get advancedSetupTitle => 'Advanced setup';
