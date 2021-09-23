992c1331061e4d018bb621887a1a145c268f6ee3
J-P Nurmi <jp.nurmi@canonical.com>
2021-09-23 09:38:42 +0200

    Add String.orIfEmpty() convenience helper
    Inspired by List.orIfNull

--- packages/ubuntu_wizard/lib/utils.dart
+++ packages/ubuntu_wizard/lib/utils.dart
@@ -4,5 +4,6 @@ export 'src/utils/equal_validator.dart';
 export 'src/utils/list_extensions.dart';
 export 'src/utils/password.dart';
 export 'src/utils/product_info_extractor.dart';
+export 'src/utils/string.dart';
 export 'src/utils/system_shutdown.dart';
 export 'src/utils/window.dart';
