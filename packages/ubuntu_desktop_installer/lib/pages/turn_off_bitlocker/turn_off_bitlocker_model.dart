8bdc762f0bc5c554384e3b4559f1008458a23b9c
J-P Nurmi <jp.nurmi@canonical.com>
2021-09-29 09:36:27 +0200

    Add logging for turn off BitLocker & RST

--- packages/ubuntu_desktop_installer/lib/pages/turn_off_bitlocker/turn_off_bitlocker_model.dart
+++ packages/ubuntu_desktop_installer/lib/pages/turn_off_bitlocker/turn_off_bitlocker_model.dart
@@ -1,10 +1,16 @@
+import 'package:logger/logger.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_wizard/utils.dart';
+
+/// @internal
+final log = Logger('turn_off_bitlocker');
 
 /// View model for [TurnOffBitLockerPage].
 class TurnOffBitLockerModel with SystemShutdown {
   /// Creates an instance with the given client.
-  TurnOffBitLockerModel(this.client);
+  TurnOffBitLockerModel(this.client) {
+    log.info('BitLocker must be turned off');
+  }
 
   @override
   final SubiquityClient client;
--- packages/ubuntu_desktop_installer/lib/pages/turn_off_rst/turn_off_rst_model.dart
+++ packages/ubuntu_desktop_installer/lib/pages/turn_off_rst/turn_off_rst_model.dart
@@ -1,10 +1,16 @@
+import 'package:logger/logger.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_wizard/utils.dart';
+
+/// @internal
+final log = Logger('turn_off_RST');
 
 /// View model for [TurnOffRSTPage].
 class TurnOffRSTModel with SystemShutdown {
   /// Creates an instance with the given client.
-  TurnOffRSTModel(this.client);
+  TurnOffRSTModel(this.client) {
+    log.info('RST must be turned off');
+  }
 
   @override
   final SubiquityClient client;
