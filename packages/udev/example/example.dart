53fb40e3d69f4e042816a53ed72f1810da0ba639
J-P Nurmi <jp.nurmi@canonical.com>
2021-09-29 17:20:57 +0200

    udev: add support for looking up block devices
    The device partitioning pages need to show pretty disk names.
    
    For example:
    
    ```dart
    final device = UdevDevice.fromSysname(udev, subsystem: 'block', sysname: 'sda');
    print('\t${device?.vendor} ${device?.model}'); // "SanDisk Ultra_Fit"
    ```

--- packages/udev/example/example.dart
+++ packages/udev/example/example.dart
@@ -9,13 +9,13 @@ void main() async {
 
   print('Wired devices:');
   for (final wired in client.devices.where((d) => d.wired != null)) {
-    final device = udev.device(syspath: wired.udi);
+    final device = UdevDevice.fromSyspath(udev, syspath: wired.udi);
     print('\t${device?.vendor} ${device?.model}');
   }
 
   print('Wireless devices:');
   for (final wireless in client.devices.where((d) => d.wireless != null)) {
-    final device = udev.device(syspath: wireless.udi);
+    final device = UdevDevice.fromSyspath(udev, syspath: wireless.udi);
     print('\t${device?.vendor} ${device?.model}');
   }
 
--- packages/udev/lib/src/udev.dart
+++ packages/udev/lib/src/udev.dart
@@ -37,16 +37,6 @@ class Udev {
     lib!.udev_unref(_udev);
     instance = null;
   }
-
-  /// Looks up the device object based on information found in `/sys/`,
-  /// annotated with properties from the udev-internal device database.
-  ///
-  /// [syspath] is any subdirectory of `/sys/`, with the restriction that a
-  /// subdirectory of `/sys/devices` (or a symlink to one) represents a real
-  /// device and as such must contain a `uevent` file.
-  UdevDevice? device({required String syspath}) {
-    return _devices[syspath] ??= UdevDevice._fromSyspath(_udev, syspath);
-  }
 }
 
 /// Provides APIs to introspect a device on the local system.
@@ -55,6 +45,38 @@ class UdevDevice {
   final Pointer<udev_device> _udev_device;
 
   UdevDevice._(this._udev_device);
+
+  /// Looks up the device object based on the provided [subsystem] and [sysname].
+  static UdevDevice? fromSysname(
+    Udev udev, {
+    required String subsystem,
+    required String sysname,
+  }) {
+    return udev._devices['$subsystem:$sysname'] ??=
+        _fromSysname(udev._udev, subsystem, sysname);
+  }
+
+  static UdevDevice? _fromSysname(
+      Pointer<udev> udev, String subsystem, String sysname) {
+    final csubsystem = subsystem.toNativeUtf8();
+    final csysname = sysname.toNativeUtf8();
+    final dev = Udev.lib!.udev_device_new_from_subsystem_sysname(
+        udev, csubsystem.cast(), csysname.cast());
+    malloc.free(csubsystem);
+    malloc.free(csysname);
+    if (dev == nullptr) return null;
+    return UdevDevice._(dev);
+  }
+
+  /// Looks up the device object based on information found in `/sys/`,
+  /// annotated with properties from the udev-internal device database.
+  ///
+  /// [syspath] is any subdirectory of `/sys/`, with the restriction that a
+  /// subdirectory of `/sys/devices` (or a symlink to one) represents a real
+  /// device and as such must contain a `uevent` file.
+  static UdevDevice? fromSyspath(Udev udev, {required String syspath}) {
+    return udev._devices[syspath] ??= _fromSyspath(udev._udev, syspath);
+  }
 
   static UdevDevice? _fromSyspath(Pointer<udev> udev, String syspath) {
     final cstr = syspath.toNativeUtf8();
@@ -66,16 +88,22 @@ class UdevDevice {
 
   void _dispose() => Udev.lib!.udev_device_unref(_udev_device);
 
-  /// Model name (`ID_MODEL_FROM_DATABASE`).
-  String get model => _getPropertyValue("ID_MODEL_FROM_DATABASE");
+  /// Model name (`ID_MODEL` or `ID_MODEL_FROM_DATABASE`).
+  String get model =>
+      _getPropertyValue('ID_MODEL_FROM_DATABASE') ??
+      _getPropertyValue('ID_MODEL') ??
+      '';
 
-  /// Vendor name (`ID_VENDOR_FROM_DATABASE`).
-  String get vendor => _getPropertyValue("ID_VENDOR_FROM_DATABASE");
+  /// Vendor name (`ID_VENDOR` or `ID_VENDOR_FROM_DATABASE`).
+  String get vendor =>
+      _getPropertyValue('ID_VENDOR_FROM_DATABASE') ??
+      _getPropertyValue('ID_VENDOR') ??
+      '';
 
-  String _getPropertyValue(String key) {
+  String? _getPropertyValue(String key) {
     final ckey = key.toNativeUtf8().cast<Int8>();
     final cval = Udev.lib!.udev_device_get_property_value(_udev_device, ckey);
-    if (cval == nullptr) return '';
+    if (cval == nullptr) return null;
     malloc.free(ckey);
     return cval.cast<Utf8>().toDartString();
   }
