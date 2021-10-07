f8bc68a15fd6034c9b8d7c8bc5ddbe87b9ef5375
J-P Nurmi <jp.nurmi@canonical.com>
2021-10-07 13:49:53 +0200

    Hide unsupported login "strategy"
    Ref: #373
    Ref: #362

--- packages/ubuntu_desktop_installer/test/who_are_you_page_test.dart
+++ packages/ubuntu_desktop_installer/test/who_are_you_page_test.dart
@@ -6,7 +6,6 @@ import 'package:provider/provider.dart';
 import 'package:ubuntu_desktop_installer/l10n.dart';
 import 'package:ubuntu_desktop_installer/pages/who_are_you/who_are_you_model.dart';
 import 'package:ubuntu_desktop_installer/pages/who_are_you/who_are_you_page.dart';
-import 'package:ubuntu_test/utils.dart';
 import 'package:ubuntu_wizard/l10n.dart';
 import 'package:ubuntu_wizard/widgets.dart';
 
@@ -183,37 +182,38 @@ void main() {
     expect(tester.widget<OutlinedButton>(continueButton).enabled, isFalse);
   });
 
-  testWidgets('login strategy', (tester) async {
-    final model = buildModel(loginStrategy: LoginStrategy.autoLogin);
-    await tester.pumpWidget(buildApp(tester, model));
+  // https://github.com/canonical/ubuntu-desktop-installer/issues/373
+  // testWidgets('login strategy', (tester) async {
+  //   final model = buildModel(loginStrategy: LoginStrategy.autoLogin);
+  //   await tester.pumpWidget(buildApp(tester, model));
 
-    final autoLoginTile = find.widgetWithText(
-      typeOf<RadioButton<LoginStrategy>>(),
-      tester.lang.whoAreYouPageAutoLogin,
-    );
-    expect(autoLoginTile, findsOneWidget);
+  //   final autoLoginTile = find.widgetWithText(
+  //     typeOf<RadioButton<LoginStrategy>>(),
+  //     tester.lang.whoAreYouPageAutoLogin,
+  //   );
+  //   expect(autoLoginTile, findsOneWidget);
 
-    final requirePasswordTile = find.widgetWithText(
-      typeOf<RadioButton<LoginStrategy>>(),
-      tester.lang.whoAreYouPageRequirePassword,
-    );
-    expect(requirePasswordTile, findsOneWidget);
+  //   final requirePasswordTile = find.widgetWithText(
+  //     typeOf<RadioButton<LoginStrategy>>(),
+  //     tester.lang.whoAreYouPageRequirePassword,
+  //   );
+  //   expect(requirePasswordTile, findsOneWidget);
 
-    expect(
-      tester.widget<RadioButton<LoginStrategy>>(autoLoginTile).groupValue,
-      LoginStrategy.autoLogin,
-    );
-    expect(
-      tester.widget<RadioButton<LoginStrategy>>(requirePasswordTile).groupValue,
-      LoginStrategy.autoLogin,
-    );
+  //   expect(
+  //     tester.widget<RadioButton<LoginStrategy>>(autoLoginTile).groupValue,
+  //     LoginStrategy.autoLogin,
+  //   );
+  //   expect(
+  //     tester.widget<RadioButton<LoginStrategy>>(requirePasswordTile).groupValue,
+  //     LoginStrategy.autoLogin,
+  //   );
 
-    when(model.loginStrategy).thenReturn(LoginStrategy.requirePassword);
+  //   when(model.loginStrategy).thenReturn(LoginStrategy.requirePassword);
 
-    await tester.tap(requirePasswordTile);
+  //   await tester.tap(requirePasswordTile);
 
-    verify(model.loginStrategy = LoginStrategy.requirePassword).called(1);
-  });
+  //   verify(model.loginStrategy = LoginStrategy.requirePassword).called(1);
+  // });
 
   testWidgets('save identity', (tester) async {
     final model = buildModel(isValid: true);
