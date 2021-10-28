e0a3b993fde570c8892ac6ea2cecd69147f7a927
J-P Nurmi <jp.nurmi@canonical.com>
2021-10-28 17:02:59 +0200

    Add parseLocale() for relaxed parsing of locales (#244)
    Ref: #241

--- packages/ubuntu_wizard/lib/utils.dart
+++ packages/ubuntu_wizard/lib/utils.dart
@@ -2,6 +2,7 @@ export 'src/utils/color.dart';
 export 'src/utils/data_size.dart';
 export 'src/utils/equal_validator.dart';
 export 'src/utils/list_extensions.dart';
+export 'src/utils/locale.dart';
 export 'src/utils/password.dart';
 export 'src/utils/product_info_extractor.dart';
 export 'src/utils/string.dart';
