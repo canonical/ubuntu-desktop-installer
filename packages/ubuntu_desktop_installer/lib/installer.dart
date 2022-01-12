b084eee258c7ad369b05ecba1b34f47352d66de4
J-P Nurmi <jp.nurmi@canonical.com>
2022-01-12 12:07:38 +0100

    Fix analysis issues
    These slipped in because the Dart SDK version and the service locator
    changes were done in different branches around the same time.

--- packages/ubuntu_desktop_installer/lib/installer.dart
+++ packages/ubuntu_desktop_installer/lib/installer.dart
@@ -34,9 +34,9 @@ void runInstallerApp(List<String> args, {FlavorData? flavor}) {
 
   registerService(() => DiskStorageService(subiquityClient));
   registerService(() => JournalService(journalUnit));
-  registerService(() => KeyboardService());
-  registerService(() => NetworkService());
-  registerService(() => UdevService());
+  registerService(KeyboardService.new);
+  registerService(NetworkService.new);
+  registerService(UdevService.new);
 
   final appStatus = ValueNotifier(AppStatus.loading);
 
