ab22d620521eebd35b946f74df96b93981f458af
J-P Nurmi <jp.nurmi@canonical.com>
2022-01-25 13:22:57 +0100

    Use ValidatedFormField.obscureText on the Who are you? page
    Notice that there is a minor behavioral change. Instead of toggling
    both fields' password visibility from one field's toggle button, each
    field has now its own toggle button.

--- packages/ubuntu_desktop_installer/lib/pages/who_are_you/who_are_you_model.dart
+++ packages/ubuntu_desktop_installer/lib/pages/who_are_you/who_are_you_model.dart
@@ -44,7 +44,6 @@ class WhoAreYouModel extends ChangeNotifier {
       _confirmedPassword,
       _loginStrategy,
       _productName,
-      _obscureText,
     ]).addListener(notifyListeners);
   }
 
@@ -57,7 +56,6 @@ class WhoAreYouModel extends ChangeNotifier {
   final _loginStrategy =
       ValueNotifier<LoginStrategy>(LoginStrategy.requirePassword);
   final _productName = ValueNotifier<String>('');
-  final _obscureText = ValueNotifier<bool>(true);
 
   /// The current real name.
   String get realName => _realName.value ?? '';
@@ -125,10 +123,6 @@ class WhoAreYouModel extends ChangeNotifier {
     log.info('Saved identity: ${identity.description}');
     return _client.setIdentity(identity);
   }
-
-  /// Defines if the password text is obscured
-  bool get obscureText => _obscureText.value;
-  set obscureText(bool value) => _obscureText.value = value;
 }
 
 /// The DMI product name file. Available for testing.
--- packages/ubuntu_desktop_installer/lib/pages/who_are_you/who_are_you_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/who_are_you/who_are_you_page.dart
@@ -5,7 +5,6 @@ import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_wizard/constants.dart';
 import 'package:ubuntu_wizard/utils.dart';
 import 'package:ubuntu_wizard/widgets.dart';
-import 'package:yaru_icons/yaru_icons.dart';
 
 import '../../l10n.dart';
 import '../../services.dart';
