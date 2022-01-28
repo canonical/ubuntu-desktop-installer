canonical/ubuntu-desktop-installer

commit 9317b4c74b4a0ea760da2b106bb7b29bd4eb7394

Resolve the location of subiquity_client without symlinks (#610)
This change makes use of the `package_config` Dart package to resolve
the location of the `subiquity_client` because the symlinks to
`subiquity_client/subiquity` and `.subiquity/socket` in the project
tree are problematic in many ways.

Not only we've had to duplicate the symlinks for both the desktop
installer and the WSL setup, but any desktop installer flavor would
have to create similar symlinks too, to be able to dry-run the
installer flavor.

Also, the symlinks are problematic for snap builds. The socket symlinks
break local (as in source-type: local) builds. Using Git as the
source-type circumvents this problem, but the drawback is that then
snapcraft doesn't pick local uncommitted changes.

Close: #477

Co-authored-by: Carlos Nihelton <carlosnsoliveira@gmail.com>

packages/subiquity_client/lib/src/server.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:http/http.dart';
 import 'package:meta/meta.dart';
+import 'package:package_config/package_config.dart';
 import 'package:path/path.dart' as p;
 import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'package:xdg_directories/xdg_directories.dart' as xdg;
@@ -35,10 +36,41 @@ abstract class SubiquityServer {
   // The name of the server's Python module.
   String get _pythonModule;
 
-  // The path of the socket in live mode.
-  String _getSocketPath(ServerMode mode) => mode == ServerMode.LIVE
-      ? '/run/subiquity/socket'
-      : p.join(Directory.current.path, 'test/socket');
+  // The path of the socket is fixed in live-run mode. In dry-run mode, it needs
+  // to be resolved based on the location of the `subiquity_client` package.
+  Future<String> _getSocketPath(ServerMode mode) async {
+    if (mode == ServerMode.DRY_RUN) {
+      return p.join(await _getSubiquityPath(), '.subiquity/socket');
+    }
+    return '/run/subiquity/socket';
+  }
+
+  String? _subiquityPath;
+
+  // Returns the location of the local subiquity submodule.
+  Future<String> _getSubiquityPath() async {
+    return _subiquityPath ??= await _findSubiquityPath();
+  }
+
+  // Finds local subiquity relative to the `subiquity_client` Dart package.
+  Future<String> _findSubiquityPath() async {
+    Object? error;
+    final config = await findPackageConfig(
+      Directory.current,
+      onError: (e) => error = e,
+    );
+    final package = config?.packages
+        .firstWhere((package) => package.name == 'subiquity_client');
+    if (package == null) {
+      log.warning(
+          'Unable to find the subiquity_client package. '
+          'Falling back to the current working dir: ${Directory.current.path}',
+          error);
+    } else {
+      log.debug('Found subiquity_client in ${package.root.path}');
+    }
+    return p.join(package?.root.path ?? Directory.current.path, 'subiquity');
+  }
 
   // Prefer local curtin and probert python modules that are pinned to the
   // correct versions
@@ -52,7 +84,7 @@ abstract class SubiquityServer {
 
   Future<String> start(ServerMode serverMode,
       {List<String>? args, Map<String, String>? environment}) async {
-    final socketPath = _getSocketPath(serverMode);
+    final socketPath = await _getSocketPath(serverMode);
     if (_shouldStart(serverMode)) {
       var subiquityCmd = <String>[
         '-m',
@@ -71,7 +103,7 @@ abstract class SubiquityServer {
 
   Future<void> _startSubiquity(
       List<String> subiquityCmd, Map<String, String>? environment) async {
-    var subiquityPath = p.join(Directory.current.path, 'subiquity');
+    final subiquityPath = await _getSubiquityPath();
     String? workingDirectory;
     // try using local subiquity
     if (Directory(subiquityPath).existsSync()) {

packages/subiquity_client/lib/src/client.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'package:dartx/dartx.dart';
 import 'package:http/http.dart';
+import 'package:path/path.dart' as p;
 import 'package:ubuntu_logger/ubuntu_logger.dart';
 import 'http_unix_client.dart';
 import 'types.dart';
@@ -34,13 +35,16 @@ class SubiquityClient {
   Future<bool> get isOpen => _isOpen.future;
 
   void open(String socketPath) {
+    // Use a relative path to avoid hitting AF_UNIX path length limit because
+    // <path/to/ubuntu-desktop-installer>/packages/subiquity_client/subiquity/.subiquity/socket>
+    // grows easily to more than 108-1 characters (char sockaddr_un::sun_path[108]).
     log.info('Opening socket $socketPath');
-    _client = HttpUnixClient(socketPath);
+    _client = HttpUnixClient(p.relative(socketPath));
     _isOpen.complete(true);
   }
 
   Future<void> close() {
-    log.info('Closing socket ${_client.path}');
+    log.info('Closing socket ${p.absolute(_client.path)}');
     return _client.flush().then((_) => _client.close());
   }
 

packages/subiquity_client/pubspec.yaml
@@ -13,6 +13,7 @@ dependencies:
   http: ^0.13.1
   json_annotation: ^4.0.1
   meta: ^1.3.0
+  package_config: ^2.0.2
   path: ^1.8.0
   synchronized: ^3.0.0
   ubuntu_logger:

packages/subiquity_client/test/socket
@@ -1,1 +1,0 @@
-../subiquity/.subiquity/socket

packages/ubuntu_desktop_installer/subiquity
@@ -1,1 +1,0 @@
-../subiquity_client/subiquity

packages/ubuntu_desktop_installer/test/socket
@@ -1,1 +1,0 @@
-../../subiquity_client/test/socket

packages/ubuntu_wsl_setup/subiquity
@@ -1,1 +1,0 @@
-../subiquity_client/subiquity

packages/ubuntu_wsl_setup/test/socket
@@ -1,1 +1,0 @@
-../../subiquity_client/test/socket

commit 26d4187bc5c7ca0e2376ecb5a1546123393c0947

Expose SubiquityServer.findSubiquityPath() for testing purposes
Integration tests were accidentally broken by #610. The tests are no
longer correctly locating the .subiquity directory. Exposing the lookup
logic in SubiquityServer allows integration tests to locate .subiquity
for verifying output files.

packages/subiquity_client/lib/src/server.dart
@@ -49,11 +49,11 @@ abstract class SubiquityServer {
 
   // Returns the location of the local subiquity submodule.
   Future<String> _getSubiquityPath() async {
-    return _subiquityPath ??= await _findSubiquityPath();
+    return _subiquityPath ??= await findSubiquityPath();
   }
 
   // Finds local subiquity relative to the `subiquity_client` Dart package.
-  Future<String> _findSubiquityPath() async {
+  static Future<String> findSubiquityPath() async {
     Object? error;
     final config = await findPackageConfig(
       Directory.current,