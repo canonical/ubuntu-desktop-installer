8a0dd00c2f78fd433ebee5d19833801296222056
Feichtmeier <frederik.feichtmeier@gmail.com>
2022-02-01 21:18:22 +0100

    ChooseYourLook: use yaru_widgets.dart for images

--- packages/ubuntu_desktop_installer/lib/pages/choose_your_look_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/choose_your_look_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:ubuntu_wizard/settings.dart';
 import 'package:ubuntu_wizard/widgets.dart';
+import 'package:yaru_widgets/yaru_widgets.dart';
 
 import '../../l10n.dart';
 
@@ -12,6 +13,7 @@ class ChooseYourLookPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final lang = AppLocalizations.of(context);
+    final width = MediaQuery.of(context).size.width;
     return WizardPage(
       header: Text(lang.chooseYourLookPageHeader),
       actions: <WizardAction>[
@@ -19,39 +21,59 @@ class ChooseYourLookPage extends StatelessWidget {
         WizardAction.next(context),
       ],
       title: Text(lang.chooseYourLookPageTitle),
-      contentPadding: const EdgeInsets.fromLTRB(20, 50, 20, 150),
       content: Center(
-        child: ListView(
-            shrinkWrap: true,
-            scrollDirection: Axis.horizontal,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: OptionCard(
-                  image: FlavorImage.asset('assets/Theme_thumbnails-Light.png'),
-                  title: Text(lang.chooseYourLookPageLightSetting),
-                  body: Text(lang.chooseYourLookPageLightBodyText),
-                  selected: Theme.of(context).brightness == Brightness.light,
-                  onSelected: () {
-                    final settings = Settings.of(context, listen: false);
-                    settings.applyTheme(Brightness.light);
-                  },
+        child: SingleChildScrollView(
+          child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(
+                      width: width / 3,
+                      child: ImageTile(
+                        path: 'assets/Theme_thumbnails-Light.png',
+                        currentlySelected:
+                            Theme.of(context).brightness == Brightness.light,
+                        onTap: () {
+                          final settings = Settings.of(context, listen: false);
+                          settings.applyTheme(Brightness.light);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(lang.chooseYourLookPageLightSetting,
+                          style: Theme.of(context).textTheme.headline6),
+                    )
+                  ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: OptionCard(
-                  image: FlavorImage.asset('assets/Theme_thumbnails-Dark.png'),
-                  title: Text(lang.chooseYourLookPageDarkSetting),
-                  body: Text(lang.chooseYourLookPageDarkBodyText),
-                  selected: Theme.of(context).brightness == Brightness.dark,
-                  onSelected: () {
-                    final settings = Settings.of(context, listen: false);
-                    settings.applyTheme(Brightness.dark);
-                  },
+                SizedBox(
+                  width: width / 20,
                 ),
-              )
-            ]),
+                Column(
+                  children: [
+                    SizedBox(
+                      width: width / 3,
+                      child: ImageTile(
+                        path: 'assets/Theme_thumbnails-Dark.png',
+                        currentlySelected:
+                            Theme.of(context).brightness == Brightness.dark,
+                        onTap: () {
+                          final settings = Settings.of(context, listen: false);
+                          settings.applyTheme(Brightness.dark);
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(lang.chooseYourLookPageDarkSetting,
+                          style: Theme.of(context).textTheme.headline6),
+                    )
+                  ],
+                )
+              ]),
+        ),
       ),
     );
   }
