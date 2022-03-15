65093bd24b85ed6be7cf28c80f23850ca3c18d01
J-P Nurmi <jp.nurmi@canonical.com>
2022-03-15 15:13:15 +0100

    Fix RadioIconTile layout
    RadioIconTile was originally added for aligning arbitrary widgets with
    RadioListTile. It has been replaced by RadioButton, which has similar
    but not identical layout. This change fixes the layout mismatch by
    adapting RadioButton's layout to RadioIconTile, and then reusing it for
    implementing RadioButton's and CheckButton's layout. This eliminates a
    nice chunk of duplication between RadioButton and CheckButton, and
    ensures that all three stay in sync.

--- packages/ubuntu_wizard/lib/src/widgets/radio_icon_tile.dart
+++ packages/ubuntu_wizard/lib/src/widgets/radio_icon_tile.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 
 import '../../constants.dart';
 
-/// List tile with an icon that matches the geometry of [RadioListTile]'s radio
+const _kHorizontalSpacing = 8.0;
+const _kVerticalSpacing = 2.0;
+
+/// List tile with an icon that matches the geometry of [RadioButton]'s radio
 /// indicator.
 class RadioIconTile extends StatelessWidget {
   /// Creates a radio icon tile.
@@ -10,34 +13,71 @@ class RadioIconTile extends StatelessWidget {
     Key? key,
     this.icon,
     this.title,
+    this.subtitle,
     this.contentPadding,
+    this.enabled = true,
   }) : super(key: key);
 
-  /// An icon shown centered within the bounding box of [RadioListTile]'s radio
+  /// An icon shown centered within the bounding box of [RadioButton]'s radio
   /// indicator.
   final Widget? icon;
 
   /// The primary content of the list tile.
   final Widget? title;
 
+  /// The secondary content of the list tile.
+  final Widget? subtitle;
+
   /// The tile's internal padding.
   final EdgeInsetsGeometry? contentPadding;
 
-  Size _calculateIconSize(BuildContext context) {
-    final theme = Theme.of(context);
-    final visualDensity = theme.radioTheme.visualDensity ?? theme.visualDensity;
-    return kRadioSize + visualDensity.baseSizeAdjustment;
-  }
+  /// When [enabled] is false, the text color is set to [ThemeData.disabledColor].
+  final bool enabled;
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      contentPadding: contentPadding,
-      leading: SizedBox.fromSize(
-        size: _calculateIconSize(context),
-        child: Center(child: icon),
+    final textColor = !enabled ? Theme.of(context).disabledColor : null;
+    return Padding(
+      padding: contentPadding ?? EdgeInsets.zero,
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          SizedBox(
+            width: kRadioSize.width,
+            height: kRadioSize.height,
+            child: Center(child: icon),
+          ),
+          const SizedBox(width: _kHorizontalSpacing),
+          Expanded(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                if (title != null)
+                  DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .subtitle1!
+                        .copyWith(color: textColor),
+                    overflow: TextOverflow.ellipsis,
+                    child: title!,
+                  ),
+                if (title != null && subtitle != null)
+                  const SizedBox(height: _kVerticalSpacing),
+                if (subtitle != null)
+                  DefaultTextStyle(
+                    style: Theme.of(context)
+                        .textTheme
+                        .caption!
+                        .copyWith(color: textColor),
+                    overflow: TextOverflow.ellipsis,
+                    child: subtitle!,
+                  )
+              ],
+            ),
+          ),
+        ],
       ),
-      title: title,
     );
   }
 }
