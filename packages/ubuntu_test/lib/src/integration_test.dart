47e27df6d1c22337e8c42b5466a5afce51f34aac
pranavo72bex <pranav072bex@ioepc.edu.np>
2022-06-14 18:40:37 +0545

    chore : replace existing ignore_for_file with // ignore_for_file: type=lint

--- packages/ubuntu_test/lib/src/integration_test.dart
+++ packages/ubuntu_test/lib/src/integration_test.dart
@@ -10,11 +10,9 @@ import 'package:ubuntu_widgets/ubuntu_widgets.dart';
 
 import 'widget_testers.dart';
 
-// ignore_for_file: invalid_use_of_visible_for_testing_member
+// ignore_for_file: type=lint
 
 /// The path to the `.subiquity` directory.
-
-// ignore_for_file: type=lint
 
 Future<String> get subiquityPath async {
   return p.join(
