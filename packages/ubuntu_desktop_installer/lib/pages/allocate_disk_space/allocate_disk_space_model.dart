c479de8d176626ea8be3a9bfa319ef9a05ca08ef
J-P Nurmi <jp.nurmi@canonical.com>
2022-06-14 16:44:39 +0200

    Remove unused import

--- packages/ubuntu_desktop_installer/lib/pages/allocate_disk_space/allocate_disk_space_model.dart
+++ packages/ubuntu_desktop_installer/lib/pages/allocate_disk_space/allocate_disk_space_model.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:dartx/dartx.dart';
 import 'package:safe_change_notifier/safe_change_notifier.dart';
 import 'package:subiquity_client/subiquity_client.dart';
-import 'package:ubuntu_wizard/utils.dart';
 
 import '../../services.dart';
 
