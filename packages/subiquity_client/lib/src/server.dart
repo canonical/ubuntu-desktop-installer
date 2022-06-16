04baa282f640978dedccc0c1abe37b60b446680b
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-06-16 08:20:51 -0300

    Extracts the server process management.
    Mainly focused on how we start it.
    Aims to be flexible, providing an API based on factory constructors
    to mix and match command line parameters contextually so that it becomes
    easier to start subiquity in different ways, such as inside a container
    or WSL.

--- packages/subiquity_client/lib/src/server.dart
+++ packages/subiquity_client/lib/src/server.dart
@@ -1,23 +1,17 @@
 import 'dart:io';
 
 import 'package:meta/meta.dart';
-import 'package:path/path.dart' as p;
 
 import 'endpoint.dart';
 import 'server/common.dart';
+import 'server/process.dart';
 import 'server/paths.dart';
-import 'server/pidfile.dart';
 
-abstract class SubiquityServer {
-  Process? _serverProcess;
+class SubiquityServer {
+  /// An optional server launcher, should we need to start the server.
+  SubiquityProcess? launcher;
 
-  SubiquityServer._();
-
-  /// Creates a new subiquity server.
-  factory SubiquityServer() => _SubiquityServerImpl();
-
-  /// Creates a WSL variant of the server.
-  factory SubiquityServer.wsl() => _WslSubiquityServerImpl();
+  SubiquityServer({this.launcher});
 
   /// A callback for integration testing purposes. The callback is called when
   /// the server has been started and thus, the application is ready for
@@ -25,86 +19,18 @@ abstract class SubiquityServer {
   @visibleForTesting
   static void Function(Endpoint)? startupCallback;
 
-  // Whether the server process should be started in the specified mode.
-  bool _shouldStart(ServerMode mode);
-
-  // The name of the server's Python module.
-  String get _pythonModule;
-
-  // Prefer local curtin and probert python modules that are pinned to the
-  // correct versions
-  Map<String, String> _pythonPath(String subiquityPath) {
-    final pythonPath = (Platform.environment['PYTHONPATH'] ?? '').split(':');
-    pythonPath.add(subiquityPath);
-    pythonPath.add(p.join(subiquityPath, 'curtin'));
-    pythonPath.add(p.join(subiquityPath, 'probert'));
-    return {'PYTHONPATH': pythonPath.join(':')};
-  }
-
   Future<Endpoint> start(ServerMode serverMode,
       {List<String>? args, Map<String, String>? environment}) async {
     final socketPath = await getSocketPath(serverMode);
     final endpoint = Endpoint.unix(socketPath);
-    if (_shouldStart(serverMode)) {
-      var subiquityCmd = <String>[
-        '-m',
-        _pythonModule,
-        if (serverMode == ServerMode.DRY_RUN) '--dry-run',
-        ...?args,
-      ];
-      await _startSubiquity(serverMode, subiquityCmd, environment);
+    if (launcher != null) {
+      await launcher!.start(additionalArgs: args, additionalEnv: environment);
     }
 
     return _waitSubiquity(endpoint).then((_) {
       startupCallback?.call(endpoint);
       return endpoint;
     });
-  }
-
-  Future<void> _startSubiquity(ServerMode serverMode, List<String> subiquityCmd,
-      Map<String, String>? environment) async {
-    final subiquityPath = await getSubiquityPath();
-    String? workingDirectory;
-    // try using local subiquity
-    if (Directory(subiquityPath).existsSync()) {
-      workingDirectory = subiquityPath;
-    }
-
-    // kill the existing test server if it's already running, so they don't pile
-    // up on hot restarts
-    final pid = await readPidFile();
-    if (pid != null) {
-      Process.killPid(pid);
-    }
-
-    // Use `/usr/bin/python3` over `/snap/flutter/current/usr/bin/python3` when
-    // developing with flutter-snap on the desktop. This ensures that subiquity
-    // has locally installed Python module dependencies available. (#364)
-    _serverProcess = await Process.start(
-      Platform.environment['SNAP_PYTHON'] ?? '/usr/bin/python3',
-      subiquityCmd,
-      workingDirectory: workingDirectory,
-      environment: {
-        ..._pythonPath(subiquityPath),
-        if (serverMode == ServerMode.DRY_RUN) ...{
-          // so subiquity doesn't think it's some other snap (e.g. flutter or vs code)
-          'SNAP': '.',
-          'SNAP_NAME': 'subiquity',
-          'SNAP_REVISION': '',
-          'SNAP_VERSION': '',
-        },
-        ...?environment,
-      },
-    ).then((process) {
-      stdout.addStream(process.stdout);
-      stderr.addStream(process.stderr);
-      return process;
-    });
-    log.info(
-      'Starting server (PID: ${_serverProcess!.pid}) with args: $subiquityCmd',
-    );
-
-    await writePidFile(_serverProcess!.pid);
   }
 
   static Future<void> _waitSubiquity(Endpoint endpoint) async {
@@ -129,35 +55,5 @@ abstract class SubiquityServer {
     client.close();
   }
 
-  Future<void> stop() async {
-    try {
-      await pidFile().delete();
-    } on FileSystemException catch (_) {}
-    _serverProcess?.kill();
-    await _serverProcess?.exitCode;
-  }
-}
-
-class _SubiquityServerImpl extends SubiquityServer {
-  _SubiquityServerImpl() : super._();
-
-  // Normally, the server is already running in live mode and thus, only
-  // started in dry-run mode.
-  @override
-  bool _shouldStart(ServerMode mode) => mode == ServerMode.DRY_RUN;
-
-  @override
-  String get _pythonModule => 'subiquity.cmd.server';
-}
-
-// A server that runs in a WSL environment.
-class _WslSubiquityServerImpl extends SubiquityServer {
-  _WslSubiquityServerImpl() : super._();
-
-  // The server should be always started in WSL because there's no systemd.
-  @override
-  bool _shouldStart(ServerMode mode) => true;
-
-  @override
-  String get _pythonModule => 'system_setup.cmd.server';
+  Future<void> stop() async => await launcher?.stop();
 }
---
+++ packages/subiquity_client/lib/src/server/process.dart
@@ -0,0 +1,183 @@
+import 'dart:io';
+import 'package:path/path.dart' as p;
+
+import 'common.dart';
+import 'pidfile.dart';
+
+// Prefer local curtin and probert python modules that are pinned to the
+// correct versions
+Map<String, String> _pythonPath(String subiquityPath) {
+  final pythonPath = (Platform.environment['PYTHONPATH'] ?? '').split(':');
+  pythonPath.add(subiquityPath);
+  pythonPath.add(p.join(subiquityPath, 'curtin'));
+  pythonPath.add(p.join(subiquityPath, 'probert'));
+  return {'PYTHONPATH': pythonPath.join(':')};
+}
+
+/// Manages the Subiquity process.
+/// This class knows the many ways with which we may want to start Subiquity.
+/// Starting the server builds on top of [Process.start], so this launcher
+/// can be customized by implementing factory constructors that adjust the
+/// [command] and [args] (and possibly some factories to enable enhancing the
+/// command line arguments, working directory and environment with the results
+/// of asynchronous computations).
+/// The example below would allow starting subiquity inside a WSL2 distro
+/// while the GUI client is running on the Windows host.
+///
+/// ```dart
+/// /// Able to start the server inside [distroName] on WSL from the Windows host.
+/// factory SubiquityProcess.wsl(String distroName, String command,
+///     {List<String>? args,
+///     Future<void>? defer,
+///     Future<void> Function()? onProcessStart,
+///     Future<List<String>> Function()? argumentsFactory}) {
+///   return SubiquityProcess._(
+///     'wsl',
+///     args: ['-d', distroName, command, ...?args],
+///     deferStart: defer,
+///     argsFactory: argumentsFactory,
+///     onProcessStart: onProcessStart,
+///   );
+/// }
+/// ```
+///
+/// The following would allow starting subiquity inside an LXD container while
+/// the GUI is running on the Linux host, which could be good for testing.
+///
+///```dart
+/// /// Able to start the server inside [container] on LXD from the Linux host.
+/// factory SubiquityProcess.lxd(String container, String command,
+///     {List<String>? args, bool runAsDaemon = false, String? configFile}) {
+///   final lxcArgs = <String>[
+///     '-n',
+///     container,
+///     runAsDaemon ? '-d' : '',
+///     if (configFile != null) ...['-f', configFile],
+///     '--',
+///     command,
+///     ...?args
+///  ];
+///  return SubiquityProcess('lxc-execute', lxcArgs);
+///}
+///```
+///
+class SubiquityProcess {
+  /// The program the process will run.
+  final String command;
+
+  /// The program arguments.
+  final List<String> args;
+
+  /// The process working directory
+  final String? workingDirectory;
+
+  /// The process environment variables.
+  final Map<String, String>? environment;
+
+  /// An optional future that must be awaited on before starting the server.
+  /// It may be used to prevent starting the server before a certain condition
+  /// is met or a certain event happens.
+  final Future<void>? deferStart;
+
+  /// An action one may want to take after calling [Process.start].
+  final Future<void> Function()? onProcessStart;
+
+  /// The process created by the [start] method.
+  Process? _serverProcess;
+
+  // TODO: remove this ignore lint clauses at next.
+  SubiquityProcess(
+    this.command,
+    this.args, {
+    this.workingDirectory,
+    this.environment,
+    // ignore: unused_element
+    this.deferStart,
+    // ignore: unused_element
+    this.onProcessStart,
+  });
+
+  /// Able to start subiquity by running the raw [command] with [args] without
+  /// further customisation or logic, intended for the simpler cases.
+  factory SubiquityProcess.command(String command, List<String> args) {
+    return SubiquityProcess(command, args);
+  }
+
+  /// Able to start the server module [pythonModule] using Python defined either
+  /// by [pythonExecutable] or via the SNAP_PYTHON environment variable.
+  /// Defaults to system's default (usually located at /usr/bin/python3).
+  factory SubiquityProcess.python(
+    String pythonModule, {
+    String? pythonExecutable,
+    String? subiquityPath,
+    ServerMode? serverMode,
+  }) {
+    final cmd = pythonExecutable ??
+        Platform.environment['SNAP_PYTHON'] ??
+        '/usr/bin/python3';
+
+    final arguments = ['-m', pythonModule];
+    final env = <String, String>{};
+
+    if (serverMode == ServerMode.DRY_RUN) {
+      arguments.add('--dry-run');
+      env.addAll({
+        // so subiquity doesn't think it's some other snap (e.g. flutter or vs code)
+        'SNAP': '.',
+        'SNAP_NAME': 'subiquity',
+        'SNAP_REVISION': '',
+        'SNAP_VERSION': '',
+      });
+    }
+    if (subiquityPath != null) env.addAll(_pythonPath(subiquityPath));
+    return SubiquityProcess(
+      cmd,
+      arguments,
+      // will try using local subiquity
+      workingDirectory: subiquityPath,
+      environment: env,
+    );
+  }
+
+  /// Starts the server.
+  Future<void> start({
+    List<String>? additionalArgs,
+    Map<String, String>? additionalEnv,
+  }) async {
+    // kill the existing test server if it's already running, so they don't pile
+    // up on hot restarts
+    final pid = await readPidFile();
+    if (pid != null) {
+      Process.killPid(pid);
+    }
+
+    if (deferStart != null) {
+      await deferStart;
+    }
+
+    _serverProcess = await Process.start(
+      command,
+      [...args, ...?additionalArgs],
+      workingDirectory: workingDirectory,
+      environment: {...?environment, ...?additionalEnv},
+    ).then((process) {
+      stdout.addStream(process.stdout);
+      stderr.addStream(process.stderr);
+      return process;
+    });
+    log.info('Starting server (PID: ${_serverProcess!.pid}) with args: $args');
+
+    await onProcessStart?.call();
+    await writePidFile(_serverProcess!.pid);
+  }
+
+  Future<void> stop() async {
+    try {
+      await pidFile().delete();
+    } on FileSystemException catch (_) {}
+    _serverProcess?.kill();
+    await _serverProcess?.exitCode;
+  }
+
+  Future<int>? get exitCode => _serverProcess?.exitCode;
+}
