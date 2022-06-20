5c9a256d3c4de6c7ce9103c5a3cc0c20f3c2f011
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-06-20 15:35:47 -0300

    Set the port range according to the platform
    running the test.

--- packages/ubuntu_wsl_setup/test/services_test.dart
+++ packages/ubuntu_wsl_setup/test/services_test.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
+
 import 'package:ubuntu_wsl_setup/services/tcp_helpers.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('ensures range is respected', () async {
     // The broader the range, more quickly we should exit the loop.
-    const lower = 50000;
+    final lower = Platform.isWindows ? 49152 : 32768;
     const higher = 60000;
     final socket = await getSocketHolder(lower: lower, higher: higher);
     addTearDown(socket!.close);
