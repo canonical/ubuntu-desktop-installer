92d0195be69d7bdde57489a2292f6ec09f421bb7
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-06-21 16:39:37 -0300

    TcpHelperService class to enclose
    the top level function, for future mockability.

--- packages/ubuntu_wsl_setup/lib/services/tcp_helpers.dart
+++ packages/ubuntu_wsl_setup/lib/services/tcp_helpers.dart
@@ -19,23 +19,25 @@ FutureOr<ServerSocket?> _tryBindUnsafe({
   }
 }
 
-/// Returns a ServerSocket already binded to a randomly-selected port in the
-/// open interval [lower]-[higher] of the loopback interface where the referred
-/// range is a subset of the platform ephemeral port range.
-/// On failure returns null. [maxAttempts] limits the amount of asynchronous
-/// iterations performed.
-Future<ServerSocket?> getRandomPortSocket({
-  required int lower,
-  required int higher,
-  int maxAttempts = 10,
-}) async {
-  assert(maxAttempts > 0, 'At least one attempt will be done anyway.');
-  ServerSocket? value;
-  int attemptsCount = 0;
-  await Future.doWhile(() async {
-    attemptsCount++;
-    value = await _tryBindUnsafe(lower: lower, higher: higher);
-    return value == null && attemptsCount <= maxAttempts;
-  });
-  return value;
+class TcpHelperService {
+  /// Returns a ServerSocket already binded to a randomly-selected port in the
+  /// open interval [lower]-[higher] of the loopback interface where the referred
+  /// range is a subset of the platform ephemeral port range.
+  /// On failure returns null. [maxAttempts] limits the amount of asynchronous
+  /// iterations performed.
+  static Future<ServerSocket?> getRandomPortSocket({
+    required int lower,
+    required int higher,
+    int maxAttempts = 10,
+  }) async {
+    assert(maxAttempts > 0, 'At least one attempt will be done anyway.');
+    ServerSocket? value;
+    int attemptsCount = 0;
+    await Future.doWhile(() async {
+      attemptsCount++;
+      value = await _tryBindUnsafe(lower: lower, higher: higher);
+      return value == null && attemptsCount <= maxAttempts;
+    });
+    return value;
+  }
 }
