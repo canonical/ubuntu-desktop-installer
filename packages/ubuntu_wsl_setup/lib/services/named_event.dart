2380c7dca4c851b1f8c24ddd405c81b07ce61679
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-07-08 08:47:13 -0300

    Prefer literal collections

--- packages/ubuntu_wsl_setup/lib/services/named_event.dart
+++ packages/ubuntu_wsl_setup/lib/services/named_event.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'dart:collection';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
@@ -57,5 +56,5 @@ class _NamedEventSingleton {
     _listeners[name] = listener;
   }
 
-  final _listeners = HashMap<String, void Function()>();
+  final _listeners = <String, void Function()>{};
 }
