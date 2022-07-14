93fc6112b67ae61cefc0e751a301e95cf833fc10
J-P Nurmi <jp.nurmi@canonical.com>
2022-07-14 13:52:54 +0200

    Remove unused import

--- packages/ubuntu_desktop_installer/test/installation_slides/slides_test.dart
+++ packages/ubuntu_desktop_installer/test/installation_slides/slides_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
