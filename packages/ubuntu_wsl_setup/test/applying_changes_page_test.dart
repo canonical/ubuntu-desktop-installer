3690f34a13f54009ad42933cc6e41461a4f86bfe
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-07-21 14:38:56 -0300

    Fix analyzer warnings

--- packages/ubuntu_wsl_setup/test/applying_changes_page_test.dart
+++ packages/ubuntu_wsl_setup/test/applying_changes_page_test.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
