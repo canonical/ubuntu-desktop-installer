0eb2987dd6347ec86f0c0eabcc7d3eb56ae94a27
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-07-21 18:09:00 -0300

    A short comment.

--- packages/ubuntu_wsl_setup/lib/args_common.dart
+++ packages/ubuntu_wsl_setup/lib/args_common.dart
@@ -16,6 +16,8 @@ screens, yet allowing user to overwrite any of those during setup.
 List<String>? serverArgsFromOptions(ArgResults options) {
   return <String>[
     if (options['prefill'] != null) ...['--prefill', options['prefill']],
+    // Prevents the server to go autoinstall if a previous run was.
+    // See https://github.com/canonical/ubuntu-desktop-installer/pull/1026
     if (options['reconfigure'] != null) '--autoinstall=',
   ];
 }
