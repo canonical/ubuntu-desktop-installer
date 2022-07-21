3a77988de0dcee2db65f0fda25027fea0e11de61
Carlos Nihelton <carlos.santanadeoliveira@canonical.com>
2022-07-21 14:01:40 -0300

    Regenerate mocks

--- packages/ubuntu_wsl_setup/test/applying_changes_page_test.mocks.dart
+++ packages/ubuntu_wsl_setup/test/applying_changes_page_test.mocks.dart
@@ -2,11 +2,13 @@
 // in ubuntu_wsl_setup/test/applying_changes_page_test.dart.
 // Do not manually edit this file.
 
-import 'dart:ui' as _i3;
+import 'dart:async' as _i5;
+import 'dart:ui' as _i4;
 
 import 'package:mockito/mockito.dart' as _i1;
+import 'package:subiquity_client/subiquity_client.dart' as _i2;
 import 'package:ubuntu_wsl_setup/pages/applying_changes/applying_changes_model.dart'
-    as _i2;
+    as _i3;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -18,15 +20,21 @@ import 'package:ubuntu_wsl_setup/pages/applying_changes/applying_changes_model.d
 // ignore_for_file: unnecessary_parenthesis
 // ignore_for_file: camel_case_types
 
+class _FakeSubiquityClient_0 extends _i1.Fake implements _i2.SubiquityClient {}
+
 /// A class which mocks [ApplyingChangesModel].
 ///
 /// See the documentation for Mockito's code generation for more information.
 class MockApplyingChangesModel extends _i1.Mock
-    implements _i2.ApplyingChangesModel {
+    implements _i3.ApplyingChangesModel {
   MockApplyingChangesModel() {
     _i1.throwOnMissingStub(this);
   }
 
+  @override
+  _i2.SubiquityClient get client =>
+      (super.noSuchMethod(Invocation.getter(#client),
+          returnValue: _FakeSubiquityClient_0()) as _i2.SubiquityClient);
   @override
   bool get isDisposed =>
       (super.noSuchMethod(Invocation.getter(#isDisposed), returnValue: false)
@@ -36,7 +44,7 @@ class MockApplyingChangesModel extends _i1.Mock
       (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false)
           as bool);
   @override
-  void init({_i3.VoidCallback? onDoneTransition}) => super.noSuchMethod(
+  void init({_i4.VoidCallback? onDoneTransition}) => super.noSuchMethod(
       Invocation.method(#init, [], {#onDoneTransition: onDoneTransition}),
       returnValueForMissingStub: null);
   @override
@@ -47,11 +55,21 @@ class MockApplyingChangesModel extends _i1.Mock
       super.noSuchMethod(Invocation.method(#notifyListeners, []),
           returnValueForMissingStub: null);
   @override
-  void addListener(_i3.VoidCallback? listener) =>
+  void addListener(_i4.VoidCallback? listener) =>
       super.noSuchMethod(Invocation.method(#addListener, [listener]),
           returnValueForMissingStub: null);
   @override
-  void removeListener(_i3.VoidCallback? listener) =>
+  void removeListener(_i4.VoidCallback? listener) =>
       super.noSuchMethod(Invocation.method(#removeListener, [listener]),
           returnValueForMissingStub: null);
+  @override
+  _i5.Future<void> reboot({bool? immediate}) => (super.noSuchMethod(
+      Invocation.method(#reboot, [], {#immediate: immediate}),
+      returnValue: Future<void>.value(),
+      returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
+  @override
+  _i5.Future<void> shutdown({bool? immediate}) => (super.noSuchMethod(
+      Invocation.method(#shutdown, [], {#immediate: immediate}),
+      returnValue: Future<void>.value(),
+      returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
 }
