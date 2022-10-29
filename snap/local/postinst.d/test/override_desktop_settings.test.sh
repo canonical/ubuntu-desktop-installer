d3143c13c3090fd70c3321b45a22d9b2876dccca
Chai Feng <chaifeng@chaifeng.com>
2022-10-29 16:27:02 +0200

    Use tee and unignore sed

--- snap/local/postinst.d/test/override_desktop_settings.test.sh
+++ snap/local/postinst.d/test/override_desktop_settings.test.sh
@@ -8,25 +8,39 @@ test-override-desktop-settings() {
     @mocktrue test -d /usr/share/glib-2.0/schemas
     @mocktrue test -d /target//usr/share/glib-2.0/schemas
     @mocktrue id ubuntu
-    @ignore sed
+    @mock tee /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-a11y.gschema.override
 
     @run "$src/10_override_desktop_settings"
 }
 
 test-override-desktop-settings-assert() {
     sudo -u ubuntu dconf dump /org/gnome/desktop/a11y/
+    sed -i -E "s/^\[([^/]*)\]/\[org.gnome.desktop.a11y.\1\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-a11y.gschema.override
+    sed -i -E "s/^\[\/\]$/\[org.gnome.desktop.a11y\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-a11y.gschema.override
     test -s /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-a11y.gschema.override
 
     sudo -u ubuntu dconf dump /org/gnome/desktop/interface/
+    tee /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-interface.gschema.override
+    sed -i -E "s/^\[([^/]*)\]/\[org.gnome.desktop.interface.\1\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-interface.gschema.override
+    sed -i -E "s/^\[\/\]$/\[org.gnome.desktop.interface\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-interface.gschema.override
     test -s /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-interface.gschema.override
 
     sudo -u ubuntu dconf dump /org/gnome/desktop/input-sources/
+    tee /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-input-sources.gschema.override
+    sed -i -E "s/^\[([^/]*)\]/\[org.gnome.desktop.input-sources.\1\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-input-sources.gschema.override
+    sed -i -E "s/^\[\/\]$/\[org.gnome.desktop.input-sources\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-input-sources.gschema.override
     test -s /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-input-sources.gschema.override
 
     sudo -u ubuntu dconf dump /org/gnome/desktop/peripherals/
+    tee /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-peripherals.gschema.override
+    sed -i -E "s/^\[([^/]*)\]/\[org.gnome.desktop.peripherals.\1\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-peripherals.gschema.override
+    sed -i -E "s/^\[\/\]$/\[org.gnome.desktop.peripherals\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-peripherals.gschema.override
     test -s /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-peripherals.gschema.override
 
     sudo -u ubuntu dconf dump /org/gnome/desktop/wm/
+    tee /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-wm.gschema.override
+    sed -i -E "s/^\[([^/]*)\]/\[org.gnome.desktop.wm.\1\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-wm.gschema.override
+    sed -i -E "s/^\[\/\]$/\[org.gnome.desktop.wm\]/g" /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-wm.gschema.override
     test -s /target//usr/share/glib-2.0/schemas/20_ubuntu-desktop-installer-wm.gschema.override
 
     glib-compile-schemas /target//usr/share/glib-2.0/schemas
