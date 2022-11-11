bf1574df6dae02d4e8b92e5b3e1dee8fe6a3eacf
J-P Nurmi <jp.nurmi@canonical.com>
2022-11-11 10:20:20 +0100

    Adapt subiquity data type tests

--- packages/subiquity_client/test/types_test.dart
+++ packages/subiquity_client/test/types_test.dart
@@ -243,11 +243,13 @@ void main() {
       diskId: '0',
       useLvm: true,
       password: '2',
+      useTpm: false,
     );
     const json = <String, dynamic>{
       'disk_id': '0',
       'use_lvm': true,
       'password': '2',
+      'use_tpm': false,
     };
     expect(choice.toJson(), equals(json));
     expect(GuidedChoice.fromJson(json), choice);
