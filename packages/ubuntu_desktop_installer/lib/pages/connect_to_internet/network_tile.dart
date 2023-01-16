8067ffaa5d069b1569a9a5f667e0e8f67f9e65c0
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-16 10:49:50 +0100

    NetworkTile: use YaruTile (#1316)

--- packages/ubuntu_desktop_installer/lib/pages/connect_to_internet/network_tile.dart
+++ packages/ubuntu_desktop_installer/lib/pages/connect_to_internet/network_tile.dart
@@ -12,40 +12,12 @@ class NetworkTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size =
-        kCheckradioActivableAreaPadding.inflateSize(kCheckradioTogglableSize);
-    return Row(
-      children: [
-        SizedBox.fromSize(
-          size: size,
-          child: Center(child: leading),
-        ),
-        const SizedBox(width: 8),
-        Expanded(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              if (title != null)
-                DefaultTextStyle(
-                  style: Theme.of(context).textTheme.subtitle1!,
-                  child: title!,
-                ),
-              if (title != null && subtitle != null) const SizedBox(height: 2),
-              if (subtitle != null)
-                DefaultTextStyle(
-                  style: Theme.of(context).textTheme.bodyText2!,
-                  child: subtitle!,
-                ),
-            ],
-          ),
-        ),
-        const SizedBox(width: 8),
-        SizedBox.fromSize(
-          size: size,
-          child: Center(child: trailing),
-        ),
-      ],
+    return YaruTile(
+      padding: EdgeInsets.zero,
+      leading: SizedBox(width: 32, child: leading),
+      title: title ?? const SizedBox.shrink(),
+      subtitle: subtitle,
+      trailing: trailing,
     );
   }
 }
