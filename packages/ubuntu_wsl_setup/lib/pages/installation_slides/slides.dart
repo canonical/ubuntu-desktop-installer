913e5f2843b1a849e04abc51d6f81c4194937200
J-P Nurmi <jp.nurmi@canonical.com>
2023-01-24 21:26:39 +0100

    ubuntu_wsl_setup: fix deprecated_member_use
    ```
    Analyzing ubuntu_wsl_setup...
    
       info • 'bodyText2' is deprecated and shouldn't be used. Use bodyMedium instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/installation_slides_page.dart:156:56 • deprecated_member_use
       info • 'errorColor' is deprecated and shouldn't be used. Use colorScheme.error instead. This feature was deprecated after v3.3.0-0.5.pre • lib/pages/installation_slides/installation_slides_page.dart:158:49 • deprecated_member_use
       info • 'bodyText1' is deprecated and shouldn't be used. Use bodyLarge instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/installation_slides_page.dart:206:47 • deprecated_member_use
       info • 'bodyText1' is deprecated and shouldn't be used. Use bodyLarge instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/installation_slides_page.dart:235:48 • deprecated_member_use
       info • 'bodyText1' is deprecated and shouldn't be used. Use bodyLarge instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/installation_slides_page.dart:260:47 • deprecated_member_use
       info • 'headline4' is deprecated and shouldn't be used. Use headlineMedium instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/slides.dart:83:60 • deprecated_member_use
       info • 'headline6' is deprecated and shouldn't be used. Use titleLarge instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/installation_slides/slides.dart:180:8 • deprecated_member_use
       info • 'bodyText2' is deprecated and shouldn't be used. Use bodyMedium instead. This feature was deprecated after v3.1.0-0.0.pre • lib/pages/profile_setup/profile_setup_page.dart:51:51 • deprecated_member_use
    
    8 issues found. (ran in 2.4s)
    ```
    
    ```
    $ dart fix --apply --code deprecated_member_use
    Computing fixes in ubuntu_wsl_setup... 16.3s
    Applying fixes...                      0.0s
    
    lib/pages/installation_slides/installation_slides_page.dart
      deprecated_member_use • 5 fixes
    
    lib/pages/installation_slides/slides.dart
      deprecated_member_use • 2 fixes
    
    lib/pages/profile_setup/profile_setup_page.dart
      deprecated_member_use • 1 fix
    
    8 fixes made in 3 files.
    ```

--- packages/ubuntu_wsl_setup/lib/pages/installation_slides/slides.dart
+++ packages/ubuntu_wsl_setup/lib/pages/installation_slides/slides.dart
@@ -80,7 +80,7 @@ class Slide extends StatelessWidget {
                     children: [
                       Text(
                         subtitle,
-                        style: Theme.of(context).textTheme.headline4!.copyWith(
+                        style: Theme.of(context).textTheme.headlineMedium!.copyWith(
                             color: Colors.white70,
                             fontSize: 32,
                             fontWeight: FontWeight.normal),
@@ -177,7 +177,7 @@ List<Slide> theSlides(BuildContext context) {
 TextStyle _bodyTextStyle(BuildContext context) {
   return Theme.of(context)
       .textTheme
-      .headline6!
+      .titleLarge!
       .copyWith(color: Colors.white70, fontWeight: FontWeight.normal);
 }
 
