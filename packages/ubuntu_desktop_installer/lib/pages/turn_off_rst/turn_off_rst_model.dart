af83f9c5566a69796b81e581656d2827d786a3a2
J-P Nurmi <jp.nurmi@canonical.com>
2023-02-17 11:16:22 +0100

    Turn off RST: use yaru_window instead of window_manager
    Ref: #1371

--- packages/ubuntu_desktop_installer/lib/pages/turn_off_rst/turn_off_rst_model.dart
+++ packages/ubuntu_desktop_installer/lib/pages/turn_off_rst/turn_off_rst_model.dart
@@ -1,17 +1,17 @@
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_logger/ubuntu_logger.dart';
-import 'package:ubuntu_wizard/utils.dart';
 
 /// @internal
 final log = Logger('turn_off_RST');
 
 /// View model for [TurnOffRSTPage].
-class TurnOffRSTModel with SystemShutdown {
+class TurnOffRSTModel {
   /// Creates an instance with the given client.
-  TurnOffRSTModel(this.client) {
+  TurnOffRSTModel(this._client) {
     log.info('RST must be turned off');
   }
 
-  @override
-  final SubiquityClient client;
+  final SubiquityClient _client;
+
+  Future<void> reboot() => _client.reboot(immediate: true);
 }
