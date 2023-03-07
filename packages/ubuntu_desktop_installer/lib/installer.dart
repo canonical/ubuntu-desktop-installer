canonical/ubuntu-desktop-installer

commit a541288cd7e46c8c5de17fdfe8a3f5dec7b63647

Share InheritedLocale via ubuntu_wizard (#1535)

packages/ubuntu_desktop_installer/lib/installer.dart
@@ -201,42 +201,45 @@ class _UbuntuDesktopInstallerAppState extends State<UbuntuDesktopInstallerApp> {
         data: widget.flavor,
         child: SlidesContext(
           slides: widget.slides,
-          child: YaruTheme(
-            data: const YaruThemeData(
-              extensions: [
-                YaruTitleBarThemeData(
-                  backgroundColor: MaterialStatePropertyAll(Colors.transparent),
-                )
-              ],
-            ),
-            builder: (context, yaru, child) {
-              return MaterialApp(
-                locale: Settings.of(context).locale,
-                onGenerateTitle: (context) {
-                  final lang = AppLocalizations.of(context);
-                  final window = YaruWindow.of(context);
-                  window.setTitle(lang.windowTitle(widget.flavor.name));
-                  return lang.appTitle;
-                },
-                theme: widget.flavor.theme ?? yaru.theme,
-                darkTheme: widget.flavor.darkTheme ?? yaru.darkTheme,
-                debugShowCheckedModeBanner: false,
-                localizationsDelegates: <LocalizationsDelegate>[
-                  ...localizationsDelegates,
-                  ...?widget.flavor.localizationsDelegates,
+          child: InheritedLocale(
+            child: YaruTheme(
+              data: const YaruThemeData(
+                extensions: [
+                  YaruTitleBarThemeData(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.transparent),
+                  )
                 ],
-                supportedLocales: supportedLocales,
-                home: buildApp(context),
-                builder: (context, child) => Stack(
-                  children: [
-                    const Positioned.fill(
-                      child: _UbuntuDesktopInstallerBackground(),
-                    ),
-                    Positioned.fill(child: child!),
+              ),
+              builder: (context, yaru, child) {
+                return MaterialApp(
+                  locale: InheritedLocale.of(context),
+                  onGenerateTitle: (context) {
+                    final lang = AppLocalizations.of(context);
+                    final window = YaruWindow.of(context);
+                    window.setTitle(lang.windowTitle(widget.flavor.name));
+                    return lang.appTitle;
+                  },
+                  theme: widget.flavor.theme ?? yaru.theme,
+                  darkTheme: widget.flavor.darkTheme ?? yaru.darkTheme,
+                  debugShowCheckedModeBanner: false,
+                  localizationsDelegates: <LocalizationsDelegate>[
+                    ...localizationsDelegates,
+                    ...?widget.flavor.localizationsDelegates,
                   ],
-                ),
-              );
-            },
+                  supportedLocales: supportedLocales,
+                  home: buildApp(context),
+                  builder: (context, child) => Stack(
+                    children: [
+                      const Positioned.fill(
+                        child: _UbuntuDesktopInstallerBackground(),
+                      ),
+                      Positioned.fill(child: child!),
+                    ],
+                  ),
+                );
+              },
+            ),
           ),
         ),
       ),

packages/ubuntu_desktop_installer/lib/pages/try_or_install/try_or_install_page.dart
@@ -8,7 +8,6 @@ import 'package:ubuntu_wizard/widgets.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 import '../../l10n.dart';
-import '../../settings.dart';
 import 'try_or_install_model.dart';
 import 'try_or_install_widgets.dart';
 
@@ -76,7 +75,7 @@ class TryOrInstallPageState extends State<TryOrInstallPage> {
           Html(
             shrinkWrap: true,
             data: lang.releaseNotesLabel(
-                model.releaseNotesURL(Settings.of(context).locale)),
+                model.releaseNotesURL(InheritedLocale.of(context))),
             style: {
               'body': Style(margin: Margins.zero),
               'a': Style(color: context.linkColor),

packages/ubuntu_desktop_installer/lib/pages/welcome/welcome_page.dart
@@ -5,12 +5,12 @@ import 'package:scroll_to_index/scroll_to_index.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_widgets/ubuntu_widgets.dart';
 import 'package:ubuntu_wizard/constants.dart';
+import 'package:ubuntu_wizard/utils.dart';
 import 'package:ubuntu_wizard/widgets.dart';
 import 'package:yaru_widgets/yaru_widgets.dart';
 
 import '../../l10n.dart';
 import '../../services.dart';
-import '../../settings.dart';
 import 'welcome_model.dart';
 
 class WelcomePage extends StatefulWidget {
@@ -40,8 +40,7 @@ class _WelcomePageState extends State<WelcomePage> {
 
     final model = Provider.of<WelcomeModel>(context, listen: false);
     model.loadLanguages().then((_) {
-      final settings = Settings.of(context, listen: false);
-      model.selectLocale(settings.locale);
+      model.selectLocale(InheritedLocale.of(context));
 
       _selectAndScrollToLanguage(
           model.selectedLanguageIndex, AutoScrollPosition.middle);
@@ -61,8 +60,7 @@ class _WelcomePageState extends State<WelcomePage> {
     final model = context.read<WelcomeModel>();
     model.selectedLanguageIndex = index;
 
-    final settings = Settings.of(context, listen: false);
-    settings.applyLocale(model.locale(index));
+    InheritedLocale.apply(context, model.locale(index));
 
     _languageListFocusNode.requestFocus();
     _languageListScrollController.scrollToIndex(index,

packages/ubuntu_desktop_installer/lib/settings.dart
@@ -33,15 +33,4 @@ class Settings extends SafeChangeNotifier {
         break;
     }
   }
-
-  /// The current application locale.
-  Locale get locale => _locale;
-  Locale _locale = WidgetsBinding.instance.window.locale;
-
-  /// Applies the given [locale].
-  void applyLocale(Locale locale) {
-    if (_locale == locale) return;
-    _locale = locale;
-    notifyListeners();
-  }
 }

packages/ubuntu_desktop_installer/test/choose_your_look/choose_your_look_page_test.mocks.dart
@@ -3,10 +3,10 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:ui' as _i2;
+import 'dart:ui' as _i3;
 
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:ubuntu_desktop_installer/settings.dart' as _i3;
+import 'package:ubuntu_desktop_installer/settings.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -19,32 +19,14 @@ import 'package:ubuntu_desktop_installer/settings.dart' as _i3;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
-class _FakeLocale_0 extends _i1.SmartFake implements _i2.Locale {
-  _FakeLocale_0(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
 /// A class which mocks [Settings].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockSettings extends _i1.Mock implements _i3.Settings {
+class MockSettings extends _i1.Mock implements _i2.Settings {
   MockSettings() {
     _i1.throwOnMissingStub(this);
   }
 
-  @override
-  _i2.Locale get locale => (super.noSuchMethod(
-        Invocation.getter(#locale),
-        returnValue: _FakeLocale_0(
-          this,
-          Invocation.getter(#locale),
-        ),
-      ) as _i2.Locale);
   @override
   bool get hasListeners => (super.noSuchMethod(
         Invocation.getter(#hasListeners),
@@ -56,7 +38,7 @@ class MockSettings extends _i1.Mock implements _i3.Settings {
         returnValue: false,
       ) as bool);
   @override
-  void applyTheme(_i2.Brightness? brightness) => super.noSuchMethod(
+  void applyTheme(_i3.Brightness? brightness) => super.noSuchMethod(
         Invocation.method(
           #applyTheme,
           [brightness],
@@ -64,15 +46,7 @@ class MockSettings extends _i1.Mock implements _i3.Settings {
         returnValueForMissingStub: null,
       );
   @override
-  void applyLocale(_i2.Locale? locale) => super.noSuchMethod(
-        Invocation.method(
-          #applyLocale,
-          [locale],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void addListener(_i2.VoidCallback? listener) => super.noSuchMethod(
+  void addListener(_i3.VoidCallback? listener) => super.noSuchMethod(
         Invocation.method(
           #addListener,
           [listener],
@@ -80,7 +54,7 @@ class MockSettings extends _i1.Mock implements _i3.Settings {
         returnValueForMissingStub: null,
       );
   @override
-  void removeListener(_i2.VoidCallback? listener) => super.noSuchMethod(
+  void removeListener(_i3.VoidCallback? listener) => super.noSuchMethod(
         Invocation.method(
           #removeListener,
           [listener],

packages/ubuntu_desktop_installer/test/settings_test.dart
@@ -24,23 +24,4 @@ void main() {
       gsettings.set('color-scheme', DBusString('prefer-dark')),
     ]);
   });
-
-  testWidgets('init locale', (tester) async {
-    tester.binding.platformDispatcher.localeTestValue =
-        const Locale('fr', 'FR');
-    final settings = Settings(MockGSettings());
-    expect(settings.locale, equals(Locale('fr', 'FR')));
-  });
-
-  test('set locale', () {
-    final settings = Settings(MockGSettings());
-    settings.applyLocale(Locale('en', 'US'));
-    expect(settings.locale, equals(Locale('en', 'US')));
-
-    var wasNotified = false;
-    settings.addListener(() => wasNotified = true);
-    settings.applyLocale(Locale('en', 'UK'));
-    expect(settings.locale, equals(Locale('en', 'UK')));
-    expect(wasNotified, isTrue);
-  });
 }

packages/ubuntu_desktop_installer/test/try_or_install/try_or_install_page_test.dart
@@ -11,7 +11,6 @@ import 'package:ubuntu_desktop_installer/pages/try_or_install/try_or_install_pag
 import 'package:ubuntu_desktop_installer/pages/try_or_install/try_or_install_widgets.dart';
 import 'package:ubuntu_desktop_installer/routes.dart';
 import 'package:ubuntu_desktop_installer/services.dart';
-import 'package:ubuntu_desktop_installer/settings.dart';
 import 'package:ubuntu_test/utils.dart';
 import 'package:ubuntu_wizard/utils.dart';
 import 'package:ubuntu_wizard/widgets.dart';
@@ -21,44 +20,44 @@ import 'try_or_install_page_test.mocks.dart';
 
 // ignore_for_file: type=lint
 
-@GenerateMocks([Settings, UrlLauncher])
+@GenerateMocks([UrlLauncher])
 void main() {
   late MaterialApp app;
   late TryOrInstallModel model;
 
   Future<void> setUpApp(WidgetTester tester) async {
     model = TryOrInstallModel();
-    final settings = MockSettings();
-    when(settings.locale).thenReturn(Locale('en'));
 
     app = MaterialApp(
       supportedLocales: supportedLocales,
       localizationsDelegates: localizationsDelegates,
       locale: Locale('en'),
-      home: Flavor(
-        data: const FlavorData(name: 'Ubuntu'),
-        child: Wizard(
-          routes: {
-            Routes.tryOrInstall: WizardRoute(
-              builder: (_) => TryOrInstallPage(),
-              onNext: (settings) {
-                switch (model.option) {
-                  case Option.repairUbuntu:
-                    return Routes.repairUbuntu;
-                  case Option.installUbuntu:
-                    return Routes.keyboardLayout;
-                  default:
-                    return null;
-                }
-              },
-            ),
-            Routes.repairUbuntu: WizardRoute(
-              builder: (context) => Text(Routes.repairUbuntu),
-            ),
-            Routes.keyboardLayout: WizardRoute(
-              builder: (context) => Text(Routes.keyboardLayout),
-            ),
-          },
+      home: InheritedLocale(
+        child: Flavor(
+          data: const FlavorData(name: 'Ubuntu'),
+          child: Wizard(
+            routes: {
+              Routes.tryOrInstall: WizardRoute(
+                builder: (_) => TryOrInstallPage(),
+                onNext: (settings) {
+                  switch (model.option) {
+                    case Option.repairUbuntu:
+                      return Routes.repairUbuntu;
+                    case Option.installUbuntu:
+                      return Routes.keyboardLayout;
+                    default:
+                      return null;
+                  }
+                },
+              ),
+              Routes.repairUbuntu: WizardRoute(
+                builder: (context) => Text(Routes.repairUbuntu),
+              ),
+              Routes.keyboardLayout: WizardRoute(
+                builder: (context) => Text(Routes.keyboardLayout),
+              ),
+            },
+          ),
         ),
       ),
     );
@@ -66,9 +65,8 @@ void main() {
     await tester.pumpWidget(MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: model, child: app),
-        ChangeNotifierProvider<Settings>.value(value: settings),
       ],
-      child: app,
+      child: InheritedLocale(child: app),
     ));
 
     expect(find.byType(TryOrInstallPage), findsOneWidget);

packages/ubuntu_desktop_installer/test/try_or_install/try_or_install_page_test.mocks.dart
@@ -3,12 +3,10 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i5;
-import 'dart:ui' as _i2;
+import 'dart:async' as _i3;
 
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:ubuntu_desktop_installer/settings.dart' as _i3;
-import 'package:ubuntu_wizard/src/utils/url_launcher.dart' as _i4;
+import 'package:ubuntu_wizard/src/utils/url_launcher.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -21,106 +19,20 @@ import 'package:ubuntu_wizard/src/utils/url_launcher.dart' as _i4;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
-class _FakeLocale_0 extends _i1.SmartFake implements _i2.Locale {
-  _FakeLocale_0(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-/// A class which mocks [Settings].
-///
-/// See the documentation for Mockito's code generation for more information.
-class MockSettings extends _i1.Mock implements _i3.Settings {
-  MockSettings() {
-    _i1.throwOnMissingStub(this);
-  }
-
-  @override
-  _i2.Locale get locale => (super.noSuchMethod(
-        Invocation.getter(#locale),
-        returnValue: _FakeLocale_0(
-          this,
-          Invocation.getter(#locale),
-        ),
-      ) as _i2.Locale);
-  @override
-  bool get hasListeners => (super.noSuchMethod(
-        Invocation.getter(#hasListeners),
-        returnValue: false,
-      ) as bool);
-  @override
-  bool get isDisposed => (super.noSuchMethod(
-        Invocation.getter(#isDisposed),
-        returnValue: false,
-      ) as bool);
-  @override
-  void applyTheme(_i2.Brightness? brightness) => super.noSuchMethod(
-        Invocation.method(
-          #applyTheme,
-          [brightness],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void applyLocale(_i2.Locale? locale) => super.noSuchMethod(
-        Invocation.method(
-          #applyLocale,
-          [locale],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void addListener(_i2.VoidCallback? listener) => super.noSuchMethod(
-        Invocation.method(
-          #addListener,
-          [listener],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void removeListener(_i2.VoidCallback? listener) => super.noSuchMethod(
-        Invocation.method(
-          #removeListener,
-          [listener],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void dispose() => super.noSuchMethod(
-        Invocation.method(
-          #dispose,
-          [],
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  void notifyListeners() => super.noSuchMethod(
-        Invocation.method(
-          #notifyListeners,
-          [],
-        ),
-        returnValueForMissingStub: null,
-      );
-}
-
 /// A class which mocks [UrlLauncher].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockUrlLauncher extends _i1.Mock implements _i4.UrlLauncher {
+class MockUrlLauncher extends _i1.Mock implements _i2.UrlLauncher {
   MockUrlLauncher() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i5.Future<bool> launchUrl(String? url) => (super.noSuchMethod(
+  _i3.Future<bool> launchUrl(String? url) => (super.noSuchMethod(
         Invocation.method(
           #launchUrl,
           [url],
         ),
-        returnValue: _i5.Future<bool>.value(false),
-      ) as _i5.Future<bool>);
+        returnValue: _i3.Future<bool>.value(false),
+      ) as _i3.Future<bool>);
 }

commit 2edb49a21dc7f840709b297b2bcdc69df4e1c0fb

Move InheritedLocale up in the tree to avoid losing language selection
This fixes a regression introduced by #1535. The app object is rebuilt
whenever Subiquity's status changes, so we cannot store InheritedLocale
there. :(

Fix: #1550

packages/ubuntu_desktop_installer/lib/installer.dart
@@ -93,12 +93,16 @@ Future<void> runInstallerApp(
   tryRegisterService(UdevService.new);
   tryRegisterService(UrlLauncher.new);
 
+  WidgetsFlutterBinding.ensureInitialized();
+
   await runWizardApp(
-    UbuntuDesktopInstallerApp(
-      flavor: flavor,
-      slides: slides,
-      initialRoute: options['initial-route'],
-      tryOrInstall: options['try-or-install'],
+    InheritedLocale(
+      child: UbuntuDesktopInstallerApp(
+        flavor: flavor,
+        slides: slides,
+        initialRoute: options['initial-route'],
+        tryOrInstall: options['try-or-install'],
+      ),
     ),
     options: options,
     subiquityClient: subiquityClient,
@@ -194,45 +198,42 @@ class _UbuntuDesktopInstallerAppState extends State<UbuntuDesktopInstallerApp> {
         data: widget.flavor,
         child: SlidesContext(
           slides: widget.slides,
-          child: InheritedLocale(
-            child: YaruTheme(
-              data: const YaruThemeData(
-                extensions: [
-                  YaruTitleBarThemeData(
-                    backgroundColor:
-                        MaterialStatePropertyAll(Colors.transparent),
-                  )
-                ],
-              ),
-              builder: (context, yaru, child) {
-                return MaterialApp(
-                  locale: InheritedLocale.of(context),
-                  onGenerateTitle: (context) {
-                    final lang = AppLocalizations.of(context);
-                    final window = YaruWindow.of(context);
-                    window.setTitle(lang.windowTitle(widget.flavor.name));
-                    return lang.appTitle;
-                  },
-                  theme: widget.flavor.theme ?? yaru.theme,
-                  darkTheme: widget.flavor.darkTheme ?? yaru.darkTheme,
-                  debugShowCheckedModeBanner: false,
-                  localizationsDelegates: <LocalizationsDelegate>[
-                    ...localizationsDelegates,
-                    ...?widget.flavor.localizationsDelegates,
-                  ],
-                  supportedLocales: supportedLocales,
-                  home: buildApp(context),
-                  builder: (context, child) => Stack(
-                    children: [
-                      const Positioned.fill(
-                        child: _UbuntuDesktopInstallerBackground(),
-                      ),
-                      Positioned.fill(child: child!),
-                    ],
-                  ),
-                );
-              },
+          child: YaruTheme(
+            data: const YaruThemeData(
+              extensions: [
+                YaruTitleBarThemeData(
+                  backgroundColor: MaterialStatePropertyAll(Colors.transparent),
+                )
+              ],
             ),
+            builder: (context, yaru, child) {
+              return MaterialApp(
+                locale: InheritedLocale.of(context),
+                onGenerateTitle: (context) {
+                  final lang = AppLocalizations.of(context);
+                  final window = YaruWindow.of(context);
+                  window.setTitle(lang.windowTitle(widget.flavor.name));
+                  return lang.appTitle;
+                },
+                theme: widget.flavor.theme ?? yaru.theme,
+                darkTheme: widget.flavor.darkTheme ?? yaru.darkTheme,
+                debugShowCheckedModeBanner: false,
+                localizationsDelegates: <LocalizationsDelegate>[
+                  ...localizationsDelegates,
+                  ...?widget.flavor.localizationsDelegates,
+                ],
+                supportedLocales: supportedLocales,
+                home: buildApp(context),
+                builder: (context, child) => Stack(
+                  children: [
+                    const Positioned.fill(
+                      child: _UbuntuDesktopInstallerBackground(),
+                    ),
+                    Positioned.fill(child: child!),
+                  ],
+                ),
+              );
+            },
           ),
         ),
       ),