d607f9673e7fb9bc5ad3110cfde9494d5e3d495d
Dennis Loose <dennis.loose@canonical.com>
2023-03-07 18:24:40 +0100

    desktop_service: update tests

--- packages/ubuntu_desktop_installer/test/services/desktop_service_test.dart
+++ packages/ubuntu_desktop_installer/test/services/desktop_service_test.dart
@@ -6,28 +6,88 @@ import 'package:ubuntu_desktop_installer/services/desktop_service.dart';
 import 'package:ubuntu_test/mocks.dart';
 
 void main() {
+  late MockGSettings interfaceSettings;
+  late MockGSettings mediaHandlingSettings;
+  late MockGSettings sessionSettings;
+  late MockGSettings screensaverSettings;
+  late DesktopService service;
+  setUp(() {
+    interfaceSettings = MockGSettings();
+    mediaHandlingSettings = MockGSettings();
+    sessionSettings = MockGSettings();
+    screensaverSettings = MockGSettings();
+    when(interfaceSettings.set(any, any)).thenAnswer((_) async {});
+    when(mediaHandlingSettings.set(any, any)).thenAnswer((_) async {});
+    when(sessionSettings.set(any, any)).thenAnswer((_) async {});
+    when(screensaverSettings.set(any, any)).thenAnswer((_) async {});
+
+    service = GnomeService(
+        interfaceSettings: interfaceSettings,
+        mediaHandlingSettings: mediaHandlingSettings,
+        sessionSettings: sessionSettings,
+        screensaverSettings: screensaverSettings);
+  });
+
   test('set color-scheme via gsettings', () async {
-    final gsettings = MockGSettings();
-    when(gsettings.set(any, any)).thenAnswer((_) async {});
-
-    final settings = GnomeService(gsettings);
-
-    when(gsettings.get('gtk-theme'))
+    when(interfaceSettings.get('gtk-theme'))
         .thenAnswer((_) async => const DBusString('Yaru-dark'));
 
-    await settings.setTheme(Brightness.light);
+    await service.setTheme(Brightness.light);
     verifyInOrder([
-      gsettings.set('gtk-theme', const DBusString('Yaru')),
-      gsettings.set('color-scheme', const DBusString('prefer-light')),
+      interfaceSettings.set('gtk-theme', const DBusString('Yaru')),
+      interfaceSettings.set('color-scheme', const DBusString('prefer-light')),
     ]);
 
-    when(gsettings.get('gtk-theme'))
+    when(interfaceSettings.get('gtk-theme'))
         .thenAnswer((_) async => const DBusString('Yaru'));
 
-    await settings.setTheme(Brightness.dark);
+    await service.setTheme(Brightness.dark);
     verifyInOrder([
-      gsettings.set('gtk-theme', const DBusString('Yaru-dark')),
-      gsettings.set('color-scheme', const DBusString('prefer-dark')),
+      interfaceSettings.set('gtk-theme', const DBusString('Yaru-dark')),
+      interfaceSettings.set('color-scheme', const DBusString('prefer-dark')),
     ]);
+  });
+
+  test('disable automounting', () async {
+    when(mediaHandlingSettings.get('automount'))
+        .thenAnswer((_) async => const DBusBoolean(true));
+    when(mediaHandlingSettings.get('automount-open'))
+        .thenAnswer((_) async => const DBusBoolean(true));
+    when(mediaHandlingSettings.get('autorun-never'))
+        .thenAnswer((_) async => const DBusBoolean(false));
+    await service.disableAutoMounting();
+    verifyInOrder([
+      mediaHandlingSettings.set('automount', const DBusBoolean(false)),
+      mediaHandlingSettings.set('automount-open', const DBusBoolean(false)),
+      mediaHandlingSettings.set('autorun-never', const DBusBoolean(true)),
+    ]);
+    await service.close();
+    verifyInOrder([
+      mediaHandlingSettings.set('automount', const DBusBoolean(true)),
+      mediaHandlingSettings.set('automount-open', const DBusBoolean(true)),
+      mediaHandlingSettings.set('autorun-never', const DBusBoolean(false)),
+    ]);
+  });
+
+  test('disable screen blanking', () async {
+    when(sessionSettings.get('idle-delay'))
+        .thenAnswer((_) async => const DBusUint32(600));
+    await service.disableScreenBlanking();
+    verify(sessionSettings.set('idle-delay', const DBusUint32(0))).called(1);
+    await service.close();
+    verify(sessionSettings.set('idle-delay', const DBusUint32(600))).called(1);
+  });
+
+  test('disable screensaver', () async {
+    when(screensaverSettings.get('idle-activation-enabled'))
+        .thenAnswer((_) async => const DBusBoolean(true));
+    await service.disableScreensaver();
+    verify(screensaverSettings.set(
+            'idle-activation-enabled', const DBusBoolean(false)))
+        .called(1);
+    await service.close();
+    verify(screensaverSettings.set(
+            'idle-activation-enabled', const DBusBoolean(true)))
+        .called(1);
   });
 }
