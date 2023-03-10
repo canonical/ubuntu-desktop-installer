ed23964228614217a335308f13a36e029dc8cb36
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-10 06:55:40 +0100

    Regenerate l10n

--- packages/ubuntu_desktop_installer/lib/l10n/app_localizations_eo.dart
+++ packages/ubuntu_desktop_installer/lib/l10n/app_localizations_eo.dart
@@ -942,4 +942,10 @@ class AppLocalizationsEo extends AppLocalizations {
 
   @override
   String get activeDirectoryPasswordEmpty => 'Deviga';
+
+  @override
+  String get activeDirectoryErrorTitle => 'Error configuring connection to Active Directory';
+
+  @override
+  String get activeDirectoryErrorMessage => 'Sorry, Active Directory can\'t be set up at the moment. Once your system is up and running, visit <a href=\"https://help.ubuntu.com/activedirectory\">help.ubuntu.com/activedirectory</a> for help.';
 }
