ed8d8206a5341ec21e21de91d970d944d6313424
J-P Nurmi <jpnurmi@gmail.com>
2023-03-10 14:07:58 +0100

    Update packages/ubuntu_desktop_installer/lib/pages/active_directory/active_directory_page.dart
    Co-authored-by: Carlos Nihelton <carlosnsoliveira@gmail.com>

--- packages/ubuntu_desktop_installer/lib/pages/active_directory/active_directory_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/active_directory/active_directory_page.dart
@@ -74,7 +74,7 @@ class _ActiveDirectoryPageState extends State<ActiveDirectoryPage> {
             final model = context.read<ActiveDirectoryModel>();
             await model.save();
             model.getJoinResult().then((result) {
-              if (mounted && result == AdJoinResult.JOIN_ERROR) {
+              if (mounted && (result == AdJoinResult.JOIN_ERROR || result == AdJoinResult.PAM_ERROR)) {
                 showActiveDirectoryErrorDialog(context);
               }
             });
