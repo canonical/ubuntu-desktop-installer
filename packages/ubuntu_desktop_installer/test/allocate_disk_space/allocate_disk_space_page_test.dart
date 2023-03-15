47ef630378cc1594b43b56bac47430014756a425
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-15 21:09:00 +0100

    Fix test

--- packages/ubuntu_desktop_installer/test/allocate_disk_space/allocate_disk_space_page_test.dart
+++ packages/ubuntu_desktop_installer/test/allocate_disk_space/allocate_disk_space_page_test.dart
@@ -293,7 +293,7 @@ void main() {
 
     final continueButton = find.descendant(
       of: find.byType(AlertDialog),
-      matching: find.text(tester.ulang.nextLabel),
+      matching: find.text(tester.ulang.continueAction),
     );
     expect(continueButton, findsOneWidget);
 
