eb7d2ed4577e06cdc9a8c1c507b9732420432246
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-23 13:28:01 +0100

    ubuntu_desktop_installer_test.dart: fix missing import

--- packages/ubuntu_desktop_installer/integration_test/ubuntu_desktop_installer_test.dart
+++ packages/ubuntu_desktop_installer/integration_test/ubuntu_desktop_installer_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:collection/collection.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:path/path.dart' as p;
