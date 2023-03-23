95ac706884dfd62fd9bdbefb81613ed5ac2c417d
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-23 10:12:40 +0100

    Use a fake desktop service to speed up integration testing in the CI

--- packages/ubuntu_desktop_installer/test/choose_your_look/choose_your_look_page_test.mocks.dart
+++ packages/ubuntu_desktop_installer/test/choose_your_look/choose_your_look_page_test.mocks.dart
@@ -7,7 +7,7 @@ import 'dart:async' as _i3;
 import 'dart:ui' as _i4;
 
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:ubuntu_desktop_installer/services.dart' as _i2;
+import 'package:ubuntu_desktop_installer/services/desktop_service.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
