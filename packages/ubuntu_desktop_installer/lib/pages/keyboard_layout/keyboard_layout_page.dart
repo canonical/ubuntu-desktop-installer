3b5b412b2721ff9708a3a1d1ce9228acbbd5f0be
J-P Nurmi <jp.nurmi@canonical.com>
2023-03-31 18:10:58 +0200

    Remove animated auto-scrolling in language & keyboard layout lists
    Close: #1499

--- packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_page.dart
+++ packages/ubuntu_desktop_installer/lib/pages/keyboard_layout/keyboard_layout_page.dart
@@ -1,8 +1,5 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:scroll_to_index/scroll_to_index.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 import 'package:ubuntu_widgets/ubuntu_widgets.dart';
 import 'package:ubuntu_wizard/constants.dart';
@@ -13,8 +10,6 @@ import '../../l10n.dart';
 import '../../services.dart';
 import 'keyboard_layout_dialogs.dart';
 import 'keyboard_layout_model.dart';
-
-const _kScrollDuration = Duration(milliseconds: 1);
 
 class KeyboardLayoutPage extends StatefulWidget {
   const KeyboardLayoutPage({
@@ -33,38 +28,16 @@ class KeyboardLayoutPage extends StatefulWidget {
 }
 
 class _KeyboardLayoutPageState extends State<KeyboardLayoutPage> {
-  final _layoutListFocusNode = FocusNode();
-  final _layoutListScrollController = AutoScrollController();
-
-  StreamSubscription<int>? _layoutChanged;
-
   @override
   void initState() {
     super.initState();
 
     final model = Provider.of<KeyboardLayoutModel>(context, listen: false);
-    model.init().then((_) {
-      _scrollToLayout(model.selectedLayoutIndex, AutoScrollPosition.middle);
-      model.updateInputSource();
-    });
-
-    _layoutChanged = model.onLayoutSelected.listen(_scrollToLayout);
-  }
-
-  void _scrollToLayout(int index, [AutoScrollPosition? position]) {
-    if (index == -1) return;
-    _layoutListScrollController.scrollToIndex(
-      index,
-      preferPosition: position,
-      duration: _kScrollDuration,
-    );
+    model.init().then((_) => model.updateInputSource());
   }
 
   @override
   void dispose() {
-    _layoutListFocusNode.dispose();
-    _layoutListScrollController.dispose();
-    _layoutChanged?.cancel();
     super.dispose();
   }
 
@@ -103,40 +76,21 @@ class _KeyboardLayoutPageState extends State<KeyboardLayoutPage> {
               child: Row(
                 children: <Widget>[
                   Expanded(
-                    child: KeySearch(
-                      autofocus: true,
-                      focusNode: _layoutListFocusNode,
-                      onSearch: (value) {
+                    child: ListWidget.builder(
+                      selectedIndex: model.selectedLayoutIndex,
+                      itemCount: model.layoutCount,
+                      itemBuilder: (context, index) => ListTile(
+                        key: ValueKey(index),
+                        title: Text(model.layoutName(index)),
+                        selected: index == model.selectedLayoutIndex,
+                        onTap: () => model.selectLayout(index),
+                      ),
+                      onKeySearch: (value) {
                         final index = model.searchLayout(value);
                         if (index != -1) {
                           model.selectLayout(index);
                         }
                       },
-                      child: FocusTraversalGroup(
-                        policy: WidgetOrderTraversalPolicy(),
-                        child: YaruBorderContainer(
-                          clipBehavior: Clip.antiAlias,
-                          child: ListView.builder(
-                            controller: _layoutListScrollController,
-                            itemCount: model.layoutCount,
-                            itemBuilder: (context, index) {
-                              return AutoScrollTag(
-                                index: index,
-                                key: ValueKey(index),
-                                controller: _layoutListScrollController,
-                                child: ListTile(
-                                  title: Text(model.layoutName(index)),
-                                  selected: index == model.selectedLayoutIndex,
-                                  onTap: () {
-                                    model.selectLayout(index);
-                                    _layoutListFocusNode.requestFocus();
-                                  },
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ),
                     ),
                   ),
                 ],
