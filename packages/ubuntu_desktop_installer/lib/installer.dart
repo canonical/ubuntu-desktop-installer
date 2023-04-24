41fdade73a7fcfe654a73a5cda6967c3d6d669eb
J-P Nurmi <jp.nurmi@canonical.com>
2023-04-24 11:34:15 +0200

    Fetch source catalogs from Subiquity
    Don't hardcode "Normal" vs. "Minimal" but fetch the list of sources
    from Subiquity, present them in the desired order and using localized
    descriptions if available.
    
    Ref: #1841

--- packages/ubuntu_desktop_installer/lib/installer.dart
+++ packages/ubuntu_desktop_installer/lib/installer.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:collection/collection.dart';
 import 'package:dartx/dartx.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -50,7 +51,7 @@ Future<void> runInstallerApp(
         help: 'Path of the machine config (dry-run only)');
     parser.addOption('source-catalog',
         valueHelp: 'path',
-        defaultsTo: 'examples/mixed-sources.yaml',
+        defaultsTo: 'examples/desktop-sources.yaml',
         help: 'Path of the source catalog (dry-run only)');
     parser.addFlag('try-or-install', hide: true);
   })!;
@@ -336,7 +337,12 @@ class _UbuntuDesktopInstallerWizardState
     super.initState();
 
     final client = getService<SubiquityClient>();
-    client.setSource(InstallationMode.normal.source).then((_) {
+    client.source().then((value) async {
+      final source = value.sources.firstWhereOrNull((s) => s.isDefault);
+      if (source != null) {
+        await client.setSource(source.id);
+      }
+
       // Use the default values for a number of endpoints
       // for which a UI page isn't implemented yet.
       client.markConfigured([
