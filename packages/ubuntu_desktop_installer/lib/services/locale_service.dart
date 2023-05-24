345d6dd1207f1694bd0682ee2944a152c93ccac3
J-P Nurmi <jp.nurmi@canonical.com>
2023-05-24 09:27:07 +0200

    feat(locale): implement org.freedesktop.locale1 backend

--- packages/ubuntu_desktop_installer/lib/services/locale_service.dart
+++ packages/ubuntu_desktop_installer/lib/services/locale_service.dart
@@ -1,3 +1,5 @@
+import 'package:dbus/dbus.dart';
+import 'package:meta/meta.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 
 abstract class LocaleService {
@@ -15,4 +17,53 @@ class SubiquityLocaleService implements LocaleService {
 
   @override
   Future<void> setLocale(String locale) => _subiquity.setLocale(locale);
+}
+
+class XdgLocaleService implements LocaleService {
+  XdgLocaleService([@visibleForTesting DBusRemoteObject? object])
+      : _object = object ?? _createRemoteObject();
+
+  static DBusRemoteObject _createRemoteObject() {
+    return DBusRemoteObject(
+      DBusClient.system(),
+      name: 'org.freedesktop.locale1',
+      path: DBusObjectPath('/org/freedesktop/locale1'),
+    );
+  }
+
+  final DBusRemoteObject _object;
+
+  @override
+  Future<String> getLocale() => _getLocale().then((v) => v['LANG']!);
+
+  Future<Map<String, String>> _getLocale() async {
+    final vars = await _object.getProperty(
+      'org.freedesktop.locale1',
+      'Locale',
+      signature: DBusSignature.array(DBusSignature.string),
+    );
+    return Map.fromEntries(vars.asStringArray().map((e) {
+      final p = e.split('=');
+      return MapEntry(p.first, p.last);
+    }));
+  }
+
+  @override
+  Future<void> setLocale(String locale) async {
+    final vars = await _getLocale();
+    for (final k in vars.keys) {
+      vars[k] = locale;
+    }
+    await _object.callMethod(
+      'org.freedesktop.locale1',
+      'SetLocale',
+      [
+        DBusArray.string([
+          for (final k in vars.keys) '$k=${vars[k]}',
+        ]),
+        const DBusBoolean(false),
+      ],
+      replySignature: DBusSignature.empty,
+    );
+  }
 }
--- packages/ubuntu_desktop_installer/test/services/locale_service_test.dart
+++ packages/ubuntu_desktop_installer/test/services/locale_service_test.dart
@@ -1,8 +1,13 @@
+import 'package:dbus/dbus.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:subiquity_test/subiquity_test.dart';
 import 'package:ubuntu_desktop_installer/services/locale_service.dart';
 
+import 'locale_service_test.mocks.dart';
+
+@GenerateMocks([DBusRemoteObject])
 void main() {
   group('subiquity', () {
     test('get locale', () async {
@@ -25,6 +30,50 @@ void main() {
 
       await service.setLocale('en_US.UTF-8');
       verify(client.setLocale('en_US.UTF-8')).called(1);
+    });
+  });
+
+  group('xdg', () {
+    test('get locale', () async {
+      final object = MockDBusRemoteObject();
+      when(object.getProperty(
+        'org.freedesktop.locale1',
+        'Locale',
+        signature: DBusSignature.array(DBusSignature.string),
+      )).thenAnswer((_) async => DBusArray.string(['LANG=en_US.UTF-8']));
+
+      final service = XdgLocaleService(object);
+      expect(await service.getLocale(), equals('en_US.UTF-8'));
+    });
+
+    test('set locale', () async {
+      final object = MockDBusRemoteObject();
+      when(object.getProperty(
+        'org.freedesktop.locale1',
+        'Locale',
+        signature: DBusSignature.array(DBusSignature.string),
+      )).thenAnswer((_) async =>
+          DBusArray.string(['LANG=en_US.UTF-8', 'LC_MESSAGES=en_US.UTF-8']));
+
+      when(object.callMethod(
+        'org.freedesktop.locale1',
+        'SetLocale',
+        any,
+        replySignature: DBusSignature.empty,
+      )).thenAnswer((_) async => DBusMethodSuccessResponse());
+
+      final service = XdgLocaleService(object);
+      await service.setLocale('en_GB.UTF-8');
+
+      verify(object.callMethod(
+        'org.freedesktop.locale1',
+        'SetLocale',
+        [
+          DBusArray.string(['LANG=en_GB.UTF-8', 'LC_MESSAGES=en_GB.UTF-8']),
+          const DBusBoolean(false),
+        ],
+        replySignature: DBusSignature.empty,
+      )).called(1);
     });
   });
 }
