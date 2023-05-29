f61dc70e04f12dbcc83ca7d4b1c34e41f4d9fff6
J-P Nurmi <jp.nurmi@canonical.com>
2023-05-29 15:10:23 +0200

    refactor(widgets): remove unused OptionCard (former try or install)

--- packages/ubuntu_wizard/lib/widgets.dart
+++ packages/ubuntu_wizard/lib/widgets.dart
@@ -2,7 +2,6 @@ export 'src/widgets/animated_expanded.dart';
 export 'src/widgets/flavor.dart';
 export 'src/widgets/icons.dart';
 export 'src/widgets/list_widget.dart';
-export 'src/widgets/option_card.dart';
 export 'src/widgets/override_mouse_cursor.dart';
 export 'src/widgets/password_strength_label.dart';
 export 'src/widgets/validated_form_field.dart';
