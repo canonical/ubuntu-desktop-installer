9a4f81cae37f1b47025a3157bbfdc193c79c5bed
Dennis Loose <dennis.loose@canonical.com>
2023-06-06 11:18:27 +0200

    test(locale_service): use XdgLocaleService

--- packages/ubuntu_desktop_installer/test/services/locale_service_test.mocks.dart
+++ packages/ubuntu_desktop_installer/test/services/locale_service_test.mocks.dart
@@ -5,14 +5,10 @@
 // @dart=2.19
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i7;
+import 'dart:async' as _i3;
 
-import 'package:dbus/src/dbus_client.dart' as _i2;
-import 'package:dbus/src/dbus_introspect.dart' as _i4;
-import 'package:dbus/src/dbus_method_response.dart' as _i5;
-import 'package:dbus/src/dbus_remote_object.dart' as _i6;
-import 'package:dbus/src/dbus_value.dart' as _i3;
 import 'package:mockito/mockito.dart' as _i1;
+import 'package:xdg_locale/src/xdg_locale.dart' as _i2;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -25,216 +21,130 @@ import 'package:mockito/mockito.dart' as _i1;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
-class _FakeDBusClient_0 extends _i1.SmartFake implements _i2.DBusClient {
-  _FakeDBusClient_0(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-class _FakeDBusObjectPath_1 extends _i1.SmartFake
-    implements _i3.DBusObjectPath {
-  _FakeDBusObjectPath_1(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-class _FakeDBusIntrospectNode_2 extends _i1.SmartFake
-    implements _i4.DBusIntrospectNode {
-  _FakeDBusIntrospectNode_2(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-class _FakeDBusValue_3 extends _i1.SmartFake implements _i3.DBusValue {
-  _FakeDBusValue_3(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-class _FakeDBusMethodSuccessResponse_4 extends _i1.SmartFake
-    implements _i5.DBusMethodSuccessResponse {
-  _FakeDBusMethodSuccessResponse_4(
-    Object parent,
-    Invocation parentInvocation,
-  ) : super(
-          parent,
-          parentInvocation,
-        );
-}
-
-/// A class which mocks [DBusRemoteObject].
+/// A class which mocks [XdgLocaleClient].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockDBusRemoteObject extends _i1.Mock implements _i6.DBusRemoteObject {
-  MockDBusRemoteObject() {
+class MockXdgLocaleClient extends _i1.Mock implements _i2.XdgLocaleClient {
+  MockXdgLocaleClient() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
-  _i2.DBusClient get client => (super.noSuchMethod(
-        Invocation.getter(#client),
-        returnValue: _FakeDBusClient_0(
-          this,
-          Invocation.getter(#client),
-        ),
-      ) as _i2.DBusClient);
+  Map<String, String> get locale => (super.noSuchMethod(
+        Invocation.getter(#locale),
+        returnValue: <String, String>{},
+      ) as Map<String, String>);
   @override
-  String get name => (super.noSuchMethod(
-        Invocation.getter(#name),
+  String get x11Layout => (super.noSuchMethod(
+        Invocation.getter(#x11Layout),
         returnValue: '',
       ) as String);
   @override
-  _i3.DBusObjectPath get path => (super.noSuchMethod(
-        Invocation.getter(#path),
-        returnValue: _FakeDBusObjectPath_1(
-          this,
-          Invocation.getter(#path),
-        ),
-      ) as _i3.DBusObjectPath);
+  String get x11Model => (super.noSuchMethod(
+        Invocation.getter(#x11Model),
+        returnValue: '',
+      ) as String);
   @override
-  _i7.Stream<_i6.DBusPropertiesChangedSignal> get propertiesChanged =>
-      (super.noSuchMethod(
+  String get x11Variant => (super.noSuchMethod(
+        Invocation.getter(#x11Variant),
+        returnValue: '',
+      ) as String);
+  @override
+  String get x11Options => (super.noSuchMethod(
+        Invocation.getter(#x11Options),
+        returnValue: '',
+      ) as String);
+  @override
+  String get vConsoleKeymap => (super.noSuchMethod(
+        Invocation.getter(#vConsoleKeymap),
+        returnValue: '',
+      ) as String);
+  @override
+  String get vConsoleKeymapToggle => (super.noSuchMethod(
+        Invocation.getter(#vConsoleKeymapToggle),
+        returnValue: '',
+      ) as String);
+  @override
+  _i3.Stream<List<String>> get propertiesChanged => (super.noSuchMethod(
         Invocation.getter(#propertiesChanged),
-        returnValue: _i7.Stream<_i6.DBusPropertiesChangedSignal>.empty(),
-      ) as _i7.Stream<_i6.DBusPropertiesChangedSignal>);
+        returnValue: _i3.Stream<List<String>>.empty(),
+      ) as _i3.Stream<List<String>>);
   @override
-  set propertiesChanged(
-          _i7.Stream<_i6.DBusPropertiesChangedSignal>? _propertiesChanged) =>
-      super.noSuchMethod(
-        Invocation.setter(
-          #propertiesChanged,
-          _propertiesChanged,
-        ),
-        returnValueForMissingStub: null,
-      );
-  @override
-  _i7.Future<_i4.DBusIntrospectNode> introspect() => (super.noSuchMethod(
-        Invocation.method(
-          #introspect,
-          [],
-        ),
-        returnValue:
-            _i7.Future<_i4.DBusIntrospectNode>.value(_FakeDBusIntrospectNode_2(
-          this,
-          Invocation.method(
-            #introspect,
-            [],
-          ),
-        )),
-      ) as _i7.Future<_i4.DBusIntrospectNode>);
-  @override
-  _i7.Future<_i3.DBusValue> getProperty(
-    String? interface,
-    String? name, {
-    _i3.DBusSignature? signature,
-  }) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #getProperty,
-          [
-            interface,
-            name,
-          ],
-          {#signature: signature},
-        ),
-        returnValue: _i7.Future<_i3.DBusValue>.value(_FakeDBusValue_3(
-          this,
-          Invocation.method(
-            #getProperty,
-            [
-              interface,
-              name,
-            ],
-            {#signature: signature},
-          ),
-        )),
-      ) as _i7.Future<_i3.DBusValue>);
-  @override
-  _i7.Future<Map<String, _i3.DBusValue>> getAllProperties(String? interface) =>
-      (super.noSuchMethod(
-        Invocation.method(
-          #getAllProperties,
-          [interface],
-        ),
-        returnValue: _i7.Future<Map<String, _i3.DBusValue>>.value(
-            <String, _i3.DBusValue>{}),
-      ) as _i7.Future<Map<String, _i3.DBusValue>>);
-  @override
-  _i7.Future<void> setProperty(
-    String? interface,
-    String? name,
-    _i3.DBusValue? value,
+  _i3.Future<void> setLocale(
+    Map<String, String>? locale,
+    bool? interactive,
   ) =>
       (super.noSuchMethod(
         Invocation.method(
-          #setProperty,
+          #setLocale,
           [
-            interface,
-            name,
-            value,
+            locale,
+            interactive,
           ],
         ),
-        returnValue: _i7.Future<void>.value(),
-        returnValueForMissingStub: _i7.Future<void>.value(),
-      ) as _i7.Future<void>);
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
   @override
-  _i7.Future<_i5.DBusMethodSuccessResponse> callMethod(
-    String? interface,
-    String? name,
-    Iterable<_i3.DBusValue>? values, {
-    _i3.DBusSignature? replySignature,
-    bool? noReplyExpected = false,
-    bool? noAutoStart = false,
-    bool? allowInteractiveAuthorization = false,
-  }) =>
+  _i3.Future<void> setVConsoleKeyboard(
+    String? keymap,
+    String? keymapToggle,
+    bool? convert,
+    bool? interactive,
+  ) =>
       (super.noSuchMethod(
         Invocation.method(
-          #callMethod,
+          #setVConsoleKeyboard,
           [
-            interface,
-            name,
-            values,
+            keymap,
+            keymapToggle,
+            convert,
+            interactive,
           ],
-          {
-            #replySignature: replySignature,
-            #noReplyExpected: noReplyExpected,
-            #noAutoStart: noAutoStart,
-            #allowInteractiveAuthorization: allowInteractiveAuthorization,
-          },
         ),
-        returnValue: _i7.Future<_i5.DBusMethodSuccessResponse>.value(
-            _FakeDBusMethodSuccessResponse_4(
-          this,
-          Invocation.method(
-            #callMethod,
-            [
-              interface,
-              name,
-              values,
-            ],
-            {
-              #replySignature: replySignature,
-              #noReplyExpected: noReplyExpected,
-              #noAutoStart: noAutoStart,
-              #allowInteractiveAuthorization: allowInteractiveAuthorization,
-            },
-          ),
-        )),
-      ) as _i7.Future<_i5.DBusMethodSuccessResponse>);
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
+  @override
+  _i3.Future<void> setX11Keyboard(
+    String? layout,
+    String? model,
+    String? variant,
+    String? options,
+    bool? convert,
+    bool? interactive,
+  ) =>
+      (super.noSuchMethod(
+        Invocation.method(
+          #setX11Keyboard,
+          [
+            layout,
+            model,
+            variant,
+            options,
+            convert,
+            interactive,
+          ],
+        ),
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
+  @override
+  _i3.Future<void> connect() => (super.noSuchMethod(
+        Invocation.method(
+          #connect,
+          [],
+        ),
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
+  @override
+  _i3.Future<void> close() => (super.noSuchMethod(
+        Invocation.method(
+          #close,
+          [],
+        ),
+        returnValue: _i3.Future<void>.value(),
+        returnValueForMissingStub: _i3.Future<void>.value(),
+      ) as _i3.Future<void>);
 }
