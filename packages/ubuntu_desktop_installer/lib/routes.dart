8d94bf27be9c5d78f03d80c73c10c526eec8443a
J-P Nurmi <jp.nurmi@canonical.com>
2023-06-12 15:20:23 +0200

    fix(routes): fix and clean up route names
    - fix /source to match subiquity
    - remove turn off from /turn-off-bitlocker
    - remove non-existent /repair-ubuntu

--- packages/ubuntu_desktop_installer/lib/routes.dart
+++ packages/ubuntu_desktop_installer/lib/routes.dart
@@ -5,15 +5,14 @@ abstract class Routes {
   static const welcome = '/welcome';
   static const rst = '/rst';
   static const keyboard = '/keyboard';
-  static const repairUbuntu = '/repair-ubuntu';
   static const network = '/network';
   static const secureBoot = '/secure-boot';
-  static const source = '/update-other-software';
+  static const source = '/source';
   static const confirm = '/confirm';
   static const theme = '/theme';
   static const identity = '/identity';
   static const install = '/install';
-  static const bitlocker = '/turn-off-bitlocker';
+  static const bitlocker = '/bitlocker';
   static const securityKey = '/security-key';
   static const installationType = '/installation-type';
   static const selectGuidedStorage = '/select-guided-storage';
