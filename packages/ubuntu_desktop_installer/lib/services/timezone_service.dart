f6d4b52034c9dda71308bede473deab7fed1ba9c
Dennis Loose <dennis.loose@canonical.com>
2023-06-13 11:36:22 +0200

    refactor: move XdgTimezoneService to ubuntu_welcome

--- packages/ubuntu_desktop_installer/lib/services/timezone_service.dart
+++ packages/ubuntu_desktop_installer/lib/services/timezone_service.dart
@@ -1,5 +1,3 @@
-import 'package:dbus/dbus.dart';
-import 'package:meta/meta.dart';
 import 'package:subiquity_client/subiquity_client.dart';
 
 abstract class TimezoneService {
@@ -20,41 +18,5 @@ class SubiquityTimezoneService implements TimezoneService {
   @override
   Future<void> setTimezone(String timezone) {
     return _subiquity.setTimezone(timezone);
-  }
-}
-
-class XdgTimezoneService implements TimezoneService {
-  XdgTimezoneService([@visibleForTesting DBusRemoteObject? object])
-      : _object = object ?? _createRemoteObject();
-
-  static DBusRemoteObject _createRemoteObject() {
-    return DBusRemoteObject(
-      DBusClient.system(),
-      name: 'org.freedesktop.timedate1',
-      path: DBusObjectPath('/org/freedesktop/timedate1'),
-    );
-  }
-
-  final DBusRemoteObject _object;
-
-  @override
-  Future<String> getTimezone() {
-    return _object
-        .getProperty(
-          'org.freedesktop.timedate1',
-          'Timezone',
-          signature: DBusSignature.string,
-        )
-        .then((v) => v.asString());
-  }
-
-  @override
-  Future<void> setTimezone(String timezone) async {
-    await _object.callMethod(
-      'org.freedesktop.timedate1',
-      'SetTimezone',
-      [DBusString(timezone), const DBusBoolean(false)],
-      replySignature: DBusSignature.empty,
-    );
   }
 }
--- packages/ubuntu_welcome/lib/services.dart
+++ packages/ubuntu_welcome/lib/services.dart
@@ -1,3 +1,4 @@
 export 'services/xdg_identity_service.dart';
 export 'services/xdg_keyboard_service.dart';
 export 'services/xdg_locale_service.dart';
+export 'services/xdg_timezone_service.dart';
