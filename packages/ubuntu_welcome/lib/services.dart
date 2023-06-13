9fd578a45006b5d42cd813c6b4bbc2940e06ee51
Dennis Loose <dennis.loose@canonical.com>
2023-06-13 11:31:22 +0200

    refactor: move XdgLocaleService to ubuntu_welcome

--- packages/ubuntu_welcome/lib/services.dart
+++ packages/ubuntu_welcome/lib/services.dart
@@ -1,2 +1,3 @@
 export 'services/xdg_identity_service.dart';
 export 'services/xdg_keyboard_service.dart';
+export 'services/xdg_locale_service.dart';
---
+++ packages/ubuntu_welcome/lib/services/xdg_locale_service.dart
@@ -0,0 +1,23 @@
+import 'package:meta/meta.dart';
+import 'package:ubuntu_desktop_installer/services/locale_service.dart';
+import 'package:xdg_locale/xdg_locale.dart';
+
+class XdgLocaleService implements LocaleService {
+  XdgLocaleService([@visibleForTesting XdgLocaleClient? client])
+      : _client = client ?? XdgLocaleClient();
+
+  final XdgLocaleClient _client;
+  @override
+  Future<String> getLocale() async {
+    await _client.connect();
+    final locale = _client.locale['LANG']!;
+    return locale;
+  }
+
+  @override
+  Future<void> setLocale(String locale) async {
+    await _client.connect();
+    final vars = _client.locale.map((key, value) => MapEntry(key, locale));
+    await _client.setLocale(vars, false);
+  }
+}
