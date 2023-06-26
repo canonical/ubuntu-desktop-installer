9d44697f066bd73b9aa1123e8c9f51541402aeee
J-P Nurmi <jp.nurmi@canonical.com>
2023-06-26 09:51:06 +0200

    refactor(ubuntu_wizard): flatten widgets/ subdirectory

--- packages/ubuntu_wizard/lib/ubuntu_wizard.dart
+++ packages/ubuntu_wizard/lib/ubuntu_wizard.dart
@@ -1,5 +1,5 @@
-export 'src/widgets/wizard_app.dart';
-export 'src/widgets/wizard_bar.dart';
-export 'src/widgets/wizard_button.dart';
-export 'src/widgets/wizard_page.dart';
-export 'src/widgets/wizard_theme.dart';
+export 'src/wizard_app.dart';
+export 'src/wizard_bar.dart';
+export 'src/wizard_button.dart';
+export 'src/wizard_page.dart';
+export 'src/wizard_theme.dart';
