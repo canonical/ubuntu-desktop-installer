c5df0ec43f00778d8baa22bad2a33959240504f5
J-P Nurmi <jp.nurmi@canonical.com>
2023-07-05 13:06:45 +0200

    fix: add missing localization delegate

--- packages/ubuntu_welcome/lib/l10n.dart
+++ packages/ubuntu_welcome/lib/l10n.dart
@@ -2,6 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:ubuntu_desktop_installer/l10n.dart' as udi;
 import 'package:ubuntu_localizations/ubuntu_localizations.dart';
+import 'package:ubuntu_provision/l10n.dart';
 
 export 'package:flutter_gen/gen_l10n/app_localizations.dart';
 export 'package:ubuntu_localizations/ubuntu_localizations.dart';
@@ -13,6 +14,7 @@ final List<Locale> supportedLocales = {
 
 const localizationsDelegates = <LocalizationsDelegate<dynamic>>[
   ...AppLocalizations.localizationsDelegates,
+  ...UbuntuProvisionLocalizations.localizationsDelegates,
   ...udi.AppLocalizations.localizationsDelegates,
   ...GlobalUbuntuLocalizations.delegates,
 ];
