b027ff9f586cf4f7623521cb3d4ca44d55d4c83f
J-P Nurmi <jp.nurmi@canonical.com>
2023-07-18 13:55:56 +0200

    refactor(welcome): remove installer dependency - no longer needed (#2250)

--- packages/ubuntu_welcome/lib/l10n.dart
+++ packages/ubuntu_welcome/lib/l10n.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:ubuntu_desktop_installer/l10n.dart' as udi;
 import 'package:ubuntu_localizations/ubuntu_localizations.dart';
 import 'package:ubuntu_provision/l10n.dart';
 
@@ -11,6 +10,5 @@ export 'package:ubuntu_provision/l10n.dart';
 const localizationsDelegates = <LocalizationsDelegate<dynamic>>[
   ...AppLocalizations.localizationsDelegates,
   ...UbuntuProvisionLocalizations.localizationsDelegates,
-  ...udi.AppLocalizations.localizationsDelegates,
   ...GlobalUbuntuLocalizations.delegates,
 ];
