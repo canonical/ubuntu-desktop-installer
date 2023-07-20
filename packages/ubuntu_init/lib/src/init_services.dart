cbff294e68c74e643497e386eecf43f65ccb9930
J-P Nurmi <jp.nurmi@canonical.com>
2023-07-20 15:43:42 +0200

    feat(init): add helper for registering all init-phase services (#2259)

---
+++ packages/ubuntu_init/lib/src/init_services.dart
@@ -0,0 +1,37 @@
+import 'package:timezone_map/timezone_map.dart';
+import 'package:ubuntu_provision/services.dart';
+import 'package:ubuntu_service/ubuntu_service.dart';
+
+import 'services/realmd_active_directory_service.dart';
+import 'services/xdg_identity_service.dart';
+import 'services/xdg_keyboard_service.dart';
+import 'services/xdg_locale_service.dart';
+import 'services/xdg_session_service.dart';
+import 'services/xdg_timezone_service.dart';
+
+export 'package:timezone_map/timezone_map.dart' show GeoService;
+export 'services/realmd_active_directory_service.dart';
+export 'services/xdg_identity_service.dart';
+export 'services/xdg_keyboard_service.dart';
+export 'services/xdg_locale_service.dart';
+export 'services/xdg_session_service.dart';
+export 'services/xdg_timezone_service.dart';
+
+Future<void> registerInitServices() {
+  tryRegisterService<ActiveDirectoryService>(RealmdActiveDirectoryService.new);
+  tryRegisterService<IdentityService>(XdgIdentityService.new);
+  tryRegisterService<KeyboardService>(XdgKeyboardService.new);
+  tryRegisterService<LocaleService>(XdgLocaleService.new);
+  tryRegisterService<NetworkService>(NetworkService.new);
+  tryRegisterService<SessionService>(XdgSessionService.new);
+  tryRegisterService<TimezoneService>(XdgTimezoneService.new);
+
+  var geo = tryGetService<GeoService>();
+  if (geo == null) {
+    final geodata = Geodata.asset();
+    final geoname = Geoname.ubuntu(geodata: geodata);
+    geo = GeoService(sources: [geodata, geoname]);
+    registerServiceInstance(geo);
+  }
+  return geo.init();
+}
--- packages/ubuntu_init/lib/ubuntu_init.dart
+++ packages/ubuntu_init/lib/ubuntu_init.dart
@@ -1,9 +1,4 @@
 library ubuntu_init;
 
+export 'src/init_services.dart';
 export 'src/init_wizard.dart';
-export 'src/services/realmd_active_directory_service.dart';
-export 'src/services/xdg_identity_service.dart';
-export 'src/services/xdg_keyboard_service.dart';
-export 'src/services/xdg_locale_service.dart';
-export 'src/services/xdg_session_service.dart';
-export 'src/services/xdg_timezone_service.dart';
